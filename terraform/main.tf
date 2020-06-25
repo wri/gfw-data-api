47aedf9c309a0208251d8aece3e66dfc8dd96c9c
Thomas Maschler <tmaschler@wri.org>
2020-06-25 11:35:14 -0400

    update to gfw terraform modules to v0.2.1, remove redundant security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ locals {
 
 # Docker image for FastAPI app
 module "app_docker_image" {
-  source     = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/container_registry?ref=v0.2.0"
+  source     = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/container_registry?ref=v0.2.1"
   image_name = lower("${local.project}${local.name_suffix}")
   root_dir   = "${path.root}/../"
   tag        = local.container_tag
@@ -38,7 +38,7 @@ module "app_docker_image" {
 
 # Docker image for GDAL Python Batch jobs
 module "batch_gdal_python_image" {
-  source          = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/container_registry?ref=v0.2.0"
+  source          = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/container_registry?ref=v0.2.1"
   image_name      = lower("${local.project}-gdal_python${local.name_suffix}")
   root_dir        = "${path.root}/../"
   docker_path     = "batch"
@@ -47,7 +47,7 @@ module "batch_gdal_python_image" {
 
 # Docker image for PostgreSQL Client Batch jobs
 module "batch_postgresql_client_image" {
-  source          = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/container_registry?ref=v0.2.0"
+  source          = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/container_registry?ref=v0.2.1"
   image_name      = lower("${local.project}-postgresql_client${local.name_suffix}")
   root_dir        = "${path.root}/../"
   docker_path     = "batch"
@@ -56,7 +56,7 @@ module "batch_postgresql_client_image" {
 
 # Docker image for Tile Cache Batch jobs
 module "batch_tile_cache_image" {
-  source          = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/container_registry?ref=v0.2.0"
+  source          = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/container_registry?ref=v0.2.1"
   image_name      = lower("${local.project}-tile_cache${local.name_suffix}")
   root_dir        = "${path.root}/../"
   docker_path     = "batch"
@@ -65,7 +65,7 @@ module "batch_tile_cache_image" {
 
 
 module "fargate_autoscaling" {
-  source                    = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/fargate_autoscaling?ref=v0.2.0"
+  source                    = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/fargate_autoscaling?ref=v0.2.1"
   project                   = local.project
   name_suffix               = local.name_suffix
   tags                      = local.tags
@@ -82,8 +82,7 @@ module "fargate_autoscaling" {
   auto_scaling_max_cpu_util = var.auto_scaling_max_cpu_util
   auto_scaling_min_capacity = var.auto_scaling_min_capacity
   acm_certificate_arn       = var.environment == "dev" ? null : data.terraform_remote_state.core.outputs.acm_certificate
-  security_group_ids = [data.terraform_remote_state.core.outputs.postgresql_security_group_id,
-  aws_security_group.egress_https.id]
+  security_group_ids = [data.terraform_remote_state.core.outputs.postgresql_security_group_id]
   task_role_policies = [data.terraform_remote_state.core.outputs.iam_policy_s3_write_data-lake_arn, aws_iam_policy.s3_write_data-lake.arn]
   task_execution_role_policies = [data.terraform_remote_state.core.outputs.iam_policy_s3_write_data-lake_arn,
     data.terraform_remote_state.core.outputs.secrets_postgresql-reader_policy_arn,
@@ -94,7 +93,7 @@ module "fargate_autoscaling" {
 
 # Using instance types with 1 core only
 module "batch_aurora_writer" {
-  source = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/compute_environment?ref=v0.2.0"
+  source = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/compute_environment?ref=v0.2.1"
   ecs_role_policy_arns = [
     data.terraform_remote_state.core.outputs.iam_policy_s3_write_data-lake_arn,
     data.terraform_remote_state.core.outputs.secrets_postgresql-reader_policy_arn,
@@ -116,7 +115,7 @@ module "batch_aurora_writer" {
 
 
 module "batch_data_lake_writer" {
-  source = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/compute_environment?ref=v0.2.0"
+  source = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/compute_environment?ref=v0.2.1"
   ecs_role_policy_arns = [
     data.terraform_remote_state.core.outputs.iam_policy_s3_write_data-lake_arn,
     data.terraform_remote_state.core.outputs.secrets_postgresql-reader_policy_arn,
