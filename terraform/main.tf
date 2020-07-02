2d90bb5e98adab8e93d9ed550c682a44a1d58f85
Thomas Maschler <tmaschler@wri.org>
2020-07-02 02:01:39 -0400

    Allow ECS Task and Batch jobs to read all S3 objects. Open port 443 of load balancer for ingress

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ locals {
   aurora_instance_class = data.terraform_remote_state.core.outputs.aurora_cluster_instance_class
   aurora_max_vcpus      = local.aurora_instance_class == "db.t3.medium" ? 2 : local.aurora_instance_class == "db.r4.large" ? 2 : local.aurora_instance_class == "db.r4.xlarge" ? 4 : local.aurora_instance_class == "db.r4.2xlarge" ? 8 : local.aurora_instance_class == "db.r4.4xlarge" ? 16 : local.aurora_instance_class == "db.r4.8xlarge" ? 32 : local.aurora_instance_class == "db.r4.16xlarge" ? 64 : local.aurora_instance_class == "db.r5.large" ? 2 : local.aurora_instance_class == "db.r5.xlarge" ? 4 : local.aurora_instance_class == "db.r5.2xlarge" ? 8 : local.aurora_instance_class == "db.r5.4xlarge" ? 16 : local.aurora_instance_class == "db.r5.8xlarge" ? 32 : local.aurora_instance_class == "db.r5.12xlarge" ? 48 : local.aurora_instance_class == "db.r5.16xlarge" ? 64 : local.aurora_instance_class == "db.r5.24xlarge" ? 96 : ""
   service_url           = var.environment == "dev" ? "http://${module.fargate_autoscaling.lb_dns_name}" : var.service_url
-  container_tag         = substr(var.git_sha,0,7)
+  container_tag         = substr(var.git_sha, 0, 7)
 }
 
 
@@ -83,11 +83,11 @@ module "fargate_autoscaling" {
   auto_scaling_max_cpu_util = var.auto_scaling_max_cpu_util
   auto_scaling_min_capacity = var.auto_scaling_min_capacity
   acm_certificate_arn       = var.environment == "dev" ? null : data.terraform_remote_state.core.outputs.acm_certificate
-  security_group_ids = [data.terraform_remote_state.core.outputs.postgresql_security_group_id]
-  task_role_policies = [data.terraform_remote_state.core.outputs.iam_policy_s3_write_data-lake_arn, aws_iam_policy.run_batch_jobs.arn, aws_iam_policy.s3_read_only.arn]
+  security_group_ids        = [data.terraform_remote_state.core.outputs.postgresql_security_group_id]
+  task_role_policies        = [data.terraform_remote_state.core.outputs.iam_policy_s3_write_data-lake_arn, aws_iam_policy.run_batch_jobs.arn, aws_iam_policy.s3_read_only.arn]
   task_execution_role_policies = [data.terraform_remote_state.core.outputs.iam_policy_s3_write_data-lake_arn,
     data.terraform_remote_state.core.outputs.secrets_postgresql-reader_policy_arn,
-  data.terraform_remote_state.core.outputs.secrets_postgresql-writer_policy_arn,
+    data.terraform_remote_state.core.outputs.secrets_postgresql-writer_policy_arn,
   data.terraform_remote_state.core.outputs.secrets_read-gfw-api-token_policy_arn]
   container_definition = data.template_file.container_definition.rendered
 
