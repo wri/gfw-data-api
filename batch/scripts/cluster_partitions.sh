b3f4224df4129b3491fb71618a0b757fb9d12452
jterry64 <justin.terry@wri.org>
2020-07-08 12:50:58 -0400

    Fix removing partition suffix

--- batch/scripts/cluster_partitions.sh
+++ batch/scripts/cluster_partitions.sh
@@ -17,4 +17,4 @@ ME=$(basename "$0")
 # I prefer to still do it. This way, we have a consistent way to log the env variables and can make sure
 # that argument names are used consistently across all tools.
 echo "PYTHON: Cluster partitions"
-cluster_partitions.py -d "$DATASET" -v "$VERSION" -p "$PARTITION_TYPE" -P "$PARTITION_SCHEMA" -x "$INDEX_TYPE" -c "$COLUMN_NAME" -PS "$PARTITION_SUFFIX"
+cluster_partitions.py -d "$DATASET" -v "$VERSION" -p "$PARTITION_TYPE" -P "$PARTITION_SCHEMA" -x "$INDEX_TYPE" -c "$COLUMN_NAME"
