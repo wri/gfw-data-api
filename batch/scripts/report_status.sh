c0da2119b9d4898a75158ac33eaca628e8d384fe
Thomas Maschler <tmaschler@wri.org>
2020-07-08 23:28:59 -0400

    properly escape json in task report status payload, set terraform autoscaling params

--- batch/scripts/report_status.sh
+++ batch/scripts/report_status.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Use Python json.dumps to escape special characters for JSON payload
+json_escape () {
+    printf '%s' "$1" | python -c 'import json,sys; print(json.dumps(sys.stdin.read())[1:-1])'
+}
+
 # SERVICE_ACCOUNT_TOKEN, STATUS_URL are put in the env from WRITER_SECRETS
 # AWS_BATCH_JOB_ID is put in the env by AWS Batch/moto
 HEADERS="Authorization: Bearer $SERVICE_ACCOUNT_TOKEN"
@@ -21,9 +26,9 @@ echo GREP EXIT CODE: $GREP_EXIT_CODE
 # escape all quotes inside command to not break JSON payload
 # Also make sure we don't reveal any sensitive information
 # But we still want to know if the var was set
-ESC_COMMAND=$(echo "$*" | sed 's/"/\\"/g')
-ESC_OUTPUT=$(echo "$OUTPUT" | sed 's/"/\\"/g')
-ESC_OUTPUT=$(echo "$ESC_OUTPUT" | sed 's/^AWS_SECRET_ACCESS_KEY.*$/AWS_SECRET_ACCESS_KEY=\*\*\*/') # pragma: allowlist secret
+ESC_COMMAND=$(json_escape "$*")
+
+ESC_OUTPUT=$(echo "$OUTPUT" | sed 's/^AWS_SECRET_ACCESS_KEY.*$/AWS_SECRET_ACCESS_KEY=\*\*\*/') # pragma: allowlist secret
 ESC_OUTPUT=$(echo "$ESC_OUTPUT" | sed 's/^AWS_ACCESS_KEY_ID.*$/AWS_ACCESS_KEY_ID=\*\*\*/')
 ESC_OUTPUT=$(echo "$ESC_OUTPUT" | sed 's/^PGPASSWORD.*$/PGPASSWORD=\*\*\*/')  # pragma: allowlist secret
 ESC_OUTPUT=$(echo "$ESC_OUTPUT" | sed 's/^PGUSER.*$/PGUSER=\*\*\*/')
@@ -31,6 +36,7 @@ ESC_OUTPUT=$(echo "$ESC_OUTPUT" | sed 's/^PGDATABASE.*$/PGDATABASE=\*\*\*/')
 ESC_OUTPUT=$(echo "$ESC_OUTPUT" | sed 's/^PGHOST.*$/PGHOST=\*\*\*/')
 ESC_OUTPUT=$(echo "$ESC_OUTPUT" | sed 's/^SERVICE_ACCOUNT_TOKEN.*$/SERVICE_ACCOUNT_TOKEN=\*\*\*/')
 ESC_OUTPUT=$(echo "$ESC_OUTPUT" | sed 's/^GPG_KEY.*$/GPG_KEY=\*\*\*/')
+ESC_OUTPUT=$(json_escape "$ESC_OUTPUT")
 
 if [ $EXIT_CODE -eq 0 ] && [ $GREP_EXIT_CODE -ne 0 ]; then
     STATUS="success"
--- terraform/vars/terraform-dev.tfvars
+++ terraform/vars/terraform-dev.tfvars
@@ -1,4 +1,6 @@
-environment = "dev"
-log_level   = "debug"
-
-service_url = ""
+environment               = "dev"
+log_level                 = "debug"
+service_url               = ""
+desired_count             = 1
+auto_scaling_min_capacity = 1
+auto_scaling_max_capacity = 5
