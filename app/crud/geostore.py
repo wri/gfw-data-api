4a7c50fbf9ddf17f336ac19598de5b07abc5c4bc
Daniel Mannarino <daniel.mannarino@gmail.com>
2020-07-16 10:10:24 -0400

    Have userareas inherit from geostore (forgot)

--- app/crud/geostore.py
+++ app/crud/geostore.py
@@ -1,26 +1,31 @@
 import json
-import re
 from uuid import UUID
 
+from asyncpg.exceptions import UniqueViolationError
+
 from app.application import db
-from app.errors import RecordNotFoundError
+from app.errors import BadRequestError, RecordNotFoundError
 from app.models.orm.user_areas import UserArea as ORMUserArea
-from app.models.pydantic.geostore import GeostoreOut
+from app.models.pydantic.geostore import Feature, Geostore, GeostoreOut
 
 
-async def get_user_area_geostore(geostore_id: UUID) -> GeostoreOut:
+async def get_user_area_geostore(geostore_id: UUID):
     sql = f"""
         SELECT *
         FROM geostore
         WHERE gfw_geostore_id='{geostore_id}';"""
 
     row = await db.first(sql)
+
+    if row is None:
+        raise RecordNotFoundError(
+            f"Area with gfw_geostore_id {geostore_id} does not exist"
+        )
+
     print(row)
-    if row is not None:
-        geo: GeostoreOut = GeostoreOut.from_orm(row)
-        return geo
-    else:
-        raise RecordNotFoundError(f"Area with geostore_id {geostore_id} does not exist")
+    # geo: Geostore = Geostore.from_orm(row)
+
+    return row
 
 
 async def get_geostore_by_version(dataset, version, geostore_id) -> GeostoreOut:
@@ -35,57 +40,56 @@ async def get_geostore_by_version(dataset, version, geostore_id) -> GeostoreOut:
         return geo
     else:
         raise RecordNotFoundError(
-            f'Area with geostore_id {geostore_id} does not exist in "{dataset}"."{version}"'
+            f'Area with gfw_geostore_id {geostore_id} does not exist in "{dataset}"."{version}"'
         )
 
 
 async def create_user_area(**data):
     # FIXME: Check the SRID of each feature, transform if necessary
-    if not data["features"]:
-        raise Exception
-    features = data["features"]
-    features_string = f"""ST_GeomFromGeoJSON('{json.dumps(features[0]["geometry"])}')"""
-    for feature in features[1:]:
-        features_string += [f", ST_GeomFromJSON({feature['geometry']})"]
 
-    sql = f"""
-        SELECT ST_SetSRID(
-            ST_Envelope(
-                ST_Collect(
-                    {features_string}
-                )
-            ),
-            4326
-        )::Box2D;
-    """
-    result = await db.scalar(sql)
-    bbox = [float(match) for match in re.findall(r"-?\d*\.\d+|-?\d+", result)]
+    if len(data["features"]) != 1:
+        raise BadRequestError("Please submit one and only one feature per request")
 
-    area = 0
-    for feature in features:
-        area += await db.scalar(
-            f"""
-            SELECT ST_Area(
-                ST_GeomFromGeoJSON(
-                    '{json.dumps(feature["geometry"])}'
-                )
-            )
+    feature = data["features"][0]
+    geometry_str = json.dumps(feature["geometry"])
+    feature_json = json.dumps(feature, sort_keys=True)
+
+    # FIXME: The following could be far more elegant
+    bbox = await db.scalar(
+        f"""
+        SELECT ARRAY[
+            ST_XMin(ST_Envelope(ST_GeomFromGeoJSON('{geometry_str}')::geometry)),
+            ST_YMin(ST_Envelope(ST_GeomFromGeoJSON('{geometry_str}')::geometry)),
+            ST_XMax(ST_Envelope(ST_GeomFromGeoJSON('{geometry_str}')::geometry)),
+            ST_YMax(ST_Envelope(ST_GeomFromGeoJSON('{geometry_str}')::geometry))
+        ]::NUMERIC[];
         """
-        )
-    area = area / 10000
+    )
 
-    sorted_json = json.dumps(data, sort_keys=True)
+    area = await db.scalar(
+        f"""
+        SELECT ST_Area(
+            ST_GeomFromGeoJSON(
+                '{geometry_str}'
+            )
+        )
+        """
+    )
+    area = area / 10000
 
     # We could easily do this in Python but we want PostgreSQL's behavior
     # (if different) to be the source of truth.
-    # geo_id = UUID(str(hashlib.md5(sorted_json.encode("UTF-8")).hexdigest()))
-    geo_id = await db.scalar(f"SELECT MD5('{sorted_json}')::uuid;")
+    # geo_id = UUID(str(hashlib.md5(feature_json.encode("UTF-8")).hexdigest()))
+    geo_id = await db.scalar(f"SELECT MD5('{feature_json}')::uuid;")
 
-    user_area: ORMUserArea = await ORMUserArea.create(
-        gfw_geostore_id=geo_id,
-        gfw_geojson=sorted_json,
-        gfw_area__ha=area,
-        gfw_bbox=bbox,
-    )
+    try:
+        user_area: ORMUserArea = await ORMUserArea.create(
+            gfw_geostore_id=geo_id,
+            gfw_geojson=feature_json,
+            gfw_area__ha=area,
+            gfw_bbox=bbox,
+        )
+    except UniqueViolationError:
+        user_area = await get_user_area_geostore(geo_id)
 
     return user_area
