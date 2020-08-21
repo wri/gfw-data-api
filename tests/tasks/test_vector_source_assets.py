e11574b245a7597d746b0d472c3bec09f799e3ce
Thomas Maschler <tmaschler@wri.org>
2020-08-20 21:37:31 -0400

    remove fields from test, update to aws provider 3.3

--- tests/tasks/test_vector_source_assets.py
+++ tests/tasks/test_vector_source_assets.py
@@ -238,22 +238,6 @@ async def test_vector_source_asset(batch_client, async_client):
                     "is_feature_info": False,
                     "is_filter": False,
                 },
-                {
-                    "field_name": "created_on",
-                    "field_alias": "created_on",
-                    "field_description": None,
-                    "field_type": "timestamp without time zone",
-                    "is_feature_info": True,
-                    "is_filter": True,
-                },
-                {
-                    "field_name": "updated_on",
-                    "field_alias": "updated_on",
-                    "field_description": None,
-                    "field_type": "timestamp without time zone",
-                    "is_feature_info": True,
-                    "is_filter": True,
-                },
             ]
         else:
             # JSON file does not have fid field
@@ -313,22 +297,6 @@ async def test_vector_source_asset(batch_client, async_client):
                     "field_type": "ARRAY",
                     "is_feature_info": False,
                     "is_filter": False,
-                },
-                {
-                    "field_name": "created_on",
-                    "field_alias": "created_on",
-                    "field_description": None,
-                    "field_type": "timestamp without time zone",
-                    "is_feature_info": True,
-                    "is_filter": True,
-                },
-                {
-                    "field_name": "updated_on",
-                    "field_alias": "updated_on",
-                    "field_description": None,
-                    "field_type": "timestamp without time zone",
-                    "is_feature_info": True,
-                    "is_filter": True,
                 },
             ]
 
