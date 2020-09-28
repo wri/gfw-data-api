a593ab894cf8b6ed956b7cafbe67f90664194ff7
Daniel Mannarino <daniel.mannarino@gmail.com>
2020-09-28 15:02:46 -0400

    Revert an unneeded check

--- app/routes/datasets/versions.py
+++ app/routes/datasets/versions.py
@@ -88,8 +88,7 @@ async def add_new_version(
     input_data = request.dict(exclude_none=True, by_alias=True)
 
     creation_options = input_data.pop("creation_options")
-    if creation_options:
-        _verify_source_file_access(creation_options["source_uri"])
+    _verify_source_file_access(creation_options["source_uri"])
 
     # Register version with DB
     try:
