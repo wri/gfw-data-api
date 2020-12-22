15ba0e892059433899136824dcd3568b4697bd60
jterry64 <justin.terry@wri.org>
2020-12-22 11:23:34 -0500

    Fix multicolumn index

--- batch/scripts/create_index.sh
+++ batch/scripts/create_index.sh
@@ -10,7 +10,7 @@ set -e
 ME=$(basename "$0")
 . get_arguments.sh "$@"
 
-COLUMN_NAMES_UNDERSCORED="$(echo $COLUMN_NAMES | sed 's/,/_/g')"
+COLUMN_NAMES_UNDERSCORED="$(echo $COLUMN_NAMES | sed 's/,/_/g' | cut -c 1-63)"
 psql -c "CREATE INDEX IF NOT EXISTS \"${VERSION}_${COLUMN_NAMES_UNDERSCORED}_${INDEX_TYPE}_idx\"
      ON \"$DATASET\".\"$VERSION\" USING $INDEX_TYPE
      (${COLUMN_NAMES});"
