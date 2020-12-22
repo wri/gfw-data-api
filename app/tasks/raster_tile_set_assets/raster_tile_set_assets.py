23f8d10bea1f627c93b7bfc17b35628923668e69
Daniel Mannarino <daniel.mannarino@gmail.com>
2020-12-22 12:30:18 -0500

    Refactor raster_tile_set_post_completion_task a little

--- app/tasks/raster_tile_set_assets/raster_tile_set_assets.py
+++ app/tasks/raster_tile_set_assets/raster_tile_set_assets.py
@@ -68,41 +68,37 @@ async def raster_tile_set_asset(
     return log
 
 
-async def raster_tile_set_post_completion_task(asset_id: UUID):
+async def get_raster_stats(asset_id: UUID) -> List[BandStats]:
     asset_row: ORMAsset = await get_asset(asset_id)
 
-    if asset_row.creation_options["compute_stats"]:
-        # Get tiles.geojson and extent.geojson from S3
-        s3_client = get_s3_client()
-        asset_uri = get_asset_uri(
-            asset_row.dataset,
-            asset_row.version,
-            asset_row.asset_type,
-            asset_row.creation_options,
-            srid=infer_srid_from_grid(asset_row.creation_options.get("grid")),
-        )
-        # FIXME: Save extent
-        # bucket, extent_key = split_s3_path(tile_uri_to_extent_geojson(asset_uri))
-        # extent_resp = s3_client.get_object(Bucket=bucket, Key=extent_key)
-        # extent_geojson: Dict = json.loads(
-        #     extent_resp["Body"].read().decode("utf-8")
-        # )
+    compute_histogram: bool = asset_row.creation_options["compute_histogram"]
 
-        bucket, tiles_key = split_s3_path(tile_uri_to_tiles_geojson(asset_uri))
-        tiles_resp = s3_client.get_object(Bucket=bucket, Key=tiles_key)
-        tiles_geojson: dict = json.loads(tiles_resp["Body"].read().decode("utf-8"))
+    asset_uri = get_asset_uri(
+        asset_row.dataset,
+        asset_row.version,
+        asset_row.asset_type,
+        asset_row.creation_options,
+        srid=infer_srid_from_grid(asset_row.creation_options.get("grid")),
+    )
+    bucket, tiles_key = split_s3_path(tile_uri_to_tiles_geojson(asset_uri))
 
-        stats_by_band: DefaultDict[int, DefaultDict[str, List[int]]] = defaultdict(
-            lambda: defaultdict(lambda: [])
-        )
-        histogram_by_band: Dict[int, Dict[str, Any]] = dict()
+    s3_client = get_s3_client()
+    tiles_resp = s3_client.get_object(Bucket=bucket, Key=tiles_key)
+    tiles_geojson: dict = json.loads(tiles_resp["Body"].read().decode("utf-8"))
 
-        for feature in tiles_geojson["features"]:
-            for i, band in enumerate(feature["properties"]["bands"]):
-                for val in ("min", "max", "mean"):
-                    if band.get("stats", dict()).get(val) is not None:  # eliminate?
-                        stats_by_band[i][val].append(band["stats"][val])
+    stats_by_band: DefaultDict[int, DefaultDict[str, List[int]]] = defaultdict(
+        lambda: defaultdict(lambda: [])
+    )
+    histogram_by_band: Dict[int, Dict[str, Any]] = dict()
+    bandstats: List[BandStats] = []
 
+    for feature in tiles_geojson["features"]:
+        for i, band in enumerate(feature["properties"]["bands"]):
+            for val in ("min", "max", "mean"):
+                if band.get("stats", dict()).get(val) is not None:  # eliminate?
+                    stats_by_band[i][val].append(band["stats"][val])
+
+            if compute_histogram:
                 histo = band["histogram"]
                 if histogram_by_band.get(i) is None:
                     histogram_by_band[i] = {
@@ -122,7 +118,6 @@ async def raster_tile_set_post_completion_task(asset_id: UUID):
                     for bucket_num, bucket_val in enumerate(histo["buckets"]):
                         histogram_by_band[i]["value_count"][bucket_num] += bucket_val
 
-        bandstats = []
         for i, band in stats_by_band.items():
             bandstats.append(
                 BandStats(
@@ -133,4 +128,16 @@ async def raster_tile_set_post_completion_task(asset_id: UUID):
                 )
             )
 
-        _: ORMAsset = await update_asset(asset_id, stats=RasterStats(bands=bandstats))
+    return bandstats
+
+
+async def raster_tile_set_post_completion_task(asset_id: UUID):
+    asset_row: ORMAsset = await get_asset(asset_id)
+
+    if asset_row.creation_options["compute_stats"]:
+
+        stats: List[BandStats] = await get_raster_stats(asset_id)
+
+        _: ORMAsset = await update_asset(asset_id, stats=RasterStats(bands=stats))
+
+    # FIXME: Save extent in asset extent column
