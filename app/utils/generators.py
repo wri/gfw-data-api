67de50f999d33c064d067fc9908b28e03e66bbc5
Thomas Maschler <tmaschler@wri.org>
2021-07-16 13:15:29 -0400

    fix async generator

--- app/utils/generators.py
+++ app/utils/generators.py
@@ -1,7 +1,7 @@
 from typing import Any, AsyncGenerator, List
 
 
-async def list_to_async_generator(input: List[Any]) -> AsyncGenerator[Any, None]:
-    """Yield numbers from 0 to `to` every `delay` seconds."""
-    for i in input:
+async def list_to_async_generator(input_list: List[Any]) -> AsyncGenerator[Any, None]:
+    """Transform a List to an AsyncGenerator."""
+    for i in input_list:
         yield i
