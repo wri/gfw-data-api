63d0a9c1daa86066420f8750bf2ffc5359acdf2b
Justin Terry <justin.terry@wri.org>
2021-10-05 15:54:46 -0700

    Re-enable version test

--- tests_v2/unit/app/routes/datasets/test_version.py
+++ tests_v2/unit/app/routes/datasets/test_version.py
@@ -5,7 +5,7 @@ from tests_v2.unit.app.routes.utils import assert_jsend
 
 
 @pytest.mark.asyncio
-async def est_get_version(async_client: AsyncClient, generic_vector_source_version):
+async def test_get_version(async_client: AsyncClient, generic_vector_source_version):
     dataset_name, version_name, _ = generic_vector_source_version
     resp = await async_client.get(f"/dataset/{dataset_name}/{version_name}")
     assert resp.status_code == 200
