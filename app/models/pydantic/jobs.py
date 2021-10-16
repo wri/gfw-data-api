1e9f3e30af3d11b9cd0cd38c9ac28582d3754866
Daniel Mannarino <daniel.mannarino@gmail.com>
2021-10-16 17:46:36 -0400

    Fix type of num_processes Vector Tile Cache jobs

--- app/models/pydantic/jobs.py
+++ app/models/pydantic/jobs.py
@@ -115,9 +115,9 @@ class TileCacheJob(Job):
 
     job_queue = TILE_CACHE_JOB_QUEUE
     job_definition = TILE_CACHE_JOB_DEFINITION
-    vcpus = MAX_CORES / 2
+    vcpus = max(int(MAX_CORES / 2), 1)
     num_processes = max(int(MAX_CORES / 3), 1)
-    memory = MAX_MEM / 2
+    memory = max(int(MAX_CORES / 2), 1)
     attempts = 4
     attempt_duration_seconds = int(DEFAULT_JOB_DURATION * 1.5)
 
