22b5c8d65efef07fe36f63c790aceaec6241be91
Daniel Mannarino <daniel.mannarino@gmail.com>
2021-11-18 16:09:41 -0500

    Update tests for httpx changes; roll back fastapi upgrade

--- tests_v2/unit/app/test_middleware.py
+++ tests_v2/unit/app/test_middleware.py
@@ -23,34 +23,24 @@ async def test_redirect_latest(async_client, generic_vector_source_version):
     assert response.json()["data"]["is_latest"] is True
 
     # Now we the latest redirect should work
-    response = await async_client.get(
-        f"/dataset/{dataset}/latest", allow_redirects=False
-    )
+    response = await async_client.get(f"/dataset/{dataset}/latest")
     assert response.status_code == 307
     assert response.headers["location"] == f"/dataset/{dataset}/{version}"
 
     params = {"test": "query"}
-    response = await async_client.get(
-        f"/dataset/{dataset}/latest", params=params, allow_redirects=False
-    )
+    response = await async_client.get(f"/dataset/{dataset}/latest", params=params)
     assert response.status_code == 307
     assert response.headers["location"] == f"/dataset/{dataset}/{version}?test=query"
 
-    response = await async_client.post(
-        f"/dataset/{dataset}/latest/query/json", allow_redirects=False
-    )
+    response = await async_client.post(f"/dataset/{dataset}/latest/query/json")
     assert response.status_code == 307
     assert response.headers["location"] == f"/dataset/{dataset}/{version}/query/json"
 
-    response = await async_client.post(
-        f"/dataset/{dataset}/latest/download/csv", allow_redirects=False
-    )
+    response = await async_client.post(f"/dataset/{dataset}/latest/download/csv")
     assert response.status_code == 307
     assert response.headers["location"] == f"/dataset/{dataset}/{version}/download/csv"
 
     # except for POST requests which do not point to query or download
-    response = await async_client.post(
-        f"/dataset/{dataset}/latest/assets", allow_redirects=False
-    )
+    response = await async_client.post(f"/dataset/{dataset}/latest/assets")
     print(response.text)
     assert response.status_code == 400
