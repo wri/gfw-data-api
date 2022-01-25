41cb457e93e362c54f859adeeab4dc60382c59f6
Daniel Mannarino <daniel.mannarino@gmail.com>
2022-01-25 11:26:11 -0500

    Don't gratuitously require calc strings

--- app/tasks/raster_tile_cache_assets/raster_tile_cache_assets.py
+++ app/tasks/raster_tile_cache_assets/raster_tile_cache_assets.py
@@ -1,4 +1,3 @@
-import string
 from typing import Any, Dict, List, Optional
 from uuid import UUID
 
@@ -72,18 +71,7 @@ async def raster_tile_cache_asset(
     # The first thing we do for each zoom level is reproject the source asset
     # to web-mercator. We don't want the calc string (if any) used to
     # create the source asset to be applied again to the already transformed
-    # data, so set it to None. But wait, PixETL currently requires a calc
-    # string when band_count > 1, so for such cases make a generic calc
-    # string that just pass through the source bands as-is. So for a source
-    # RTS with three bands, set the calc string to "np.ma.array([A, B, C])"
-    # TODO: When pixetl allows it just set the calc string to None
-    band_count = source_asset.creation_options["band_count"]
-    if band_count > 1:
-        bands_string = str(list(string.ascii_uppercase[:band_count])).replace("'", "")
-        calc: Optional[str] = f"np.ma.array({bands_string})"
-    else:
-        calc = None
-
+    # data, so set it to None.
     source_asset_co = RasterTileSetSourceCreationOptions(
         # TODO: With python 3.9, we can use the `|` operator here
         #  waiting for https://github.com/tiangolo/uvicorn-gunicorn-fastapi-docker/pull/67
@@ -93,7 +81,7 @@ async def raster_tile_cache_asset(
                 "source_type": RasterSourceType.raster,
                 "source_driver": RasterDrivers.geotiff,
                 "source_uri": new_source_uri,
-                "calc": calc,
+                "calc": None,
                 "resampling": resampling,
                 "compute_stats": False,
                 "compute_histogram": False,
