95762c0842cf7804dbcc13cdcc7e7829e76dca9a
Daniel Mannarino <daniel.mannarino@gmail.com>
2022-03-10 12:59:03 -0500

    Fix test_asset_float for RGB model changes

--- tests/routes/datasets/test_assets.py
+++ tests/routes/datasets/test_assets.py
@@ -911,8 +911,8 @@ async def test_asset_float(async_client, batch_client, httpd):
             # uint16.max), they're based on the original breakpoints,
             # which could have been far above or below the
             # original data max and min
-            "-32766.0": {"red": 255, "green": 0, "blue": 0, "alpha": 255},
-            "98302.0": {"red": 0, "green": 0, "blue": 255, "alpha": 255},
+            "-32766.0": {"red": 255, "green": 0, "blue": 0},
+            "98302.0": {"red": 0, "green": 0, "blue": 255},
         },
     }
 
