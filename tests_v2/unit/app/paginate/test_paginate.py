132f0baed291e21fb6966659a1ac614466a573ce
gtempus <gary.tempus.jr@gmail.com>
2022-06-16 11:58:10 -0400

    feat(paginate): Use `paginate_datasets` in the `datasets` controller
    - I'm still adapting my understanding of pythonic `async/await` and what is required to support it in tests.
    - Pay special attention to the use of the words `spy` and `stub` when dealing with collaborators.
      - `spy` is used to verify that I'm calling the collaborator correctly. The test cares about nothing else. The test will fail only if the function under tests no longer upholds its part of the contract
      - `stub` is used to verify that the function under test behaves property when provided the agreed upon value of the collaborating function. It will fail only if the function under test fails to uphold its contract with callers. The `stub` means that the collaborator behavior isn't under scrutiny. Only that it continues to return an agreed upon value for the scenario at hand.
    
    GTC-1865

--- tests_v2/unit/app/paginate/test_paginate.py
+++ tests_v2/unit/app/paginate/test_paginate.py
@@ -1,26 +1,29 @@
-from typing import List
 from unittest.mock import Mock
+
+import pytest
 
 from app.crud.datasets import get_datasets
 from app.models.orm.datasets import Dataset as ORMDataset
+from app.paginate.paginate import paginate_datasets
 
 
-def paginate_datasets(crud_impl=get_datasets, size: int = None, page: int = 0) -> List[ORMDataset]:
-    crud_impl(size, page)
-    return [ORMDataset()]
-
-
-def test_that_no_pagination_happens_for_default_values():
+@pytest.mark.asyncio
+async def test_legacy_no_pagination_happens_for_default_values():
     """This is for legacy compatibility"""
     spy_get_datasets = Mock(get_datasets)
 
-    paginate_datasets(spy_get_datasets)
+    await paginate_datasets(crud_impl=spy_get_datasets)
 
     spy_get_datasets.assert_called_with(None, 0)
 
 
-def test_that_legacy_datasets_collection_is_returned():
-    result = paginate_datasets()
+@pytest.mark.asyncio
+async def test_legacy_datasets_collection_is_returned_when_no_arguments_are_given():
+    """This is for legacy compatibility"""
+    stub_get_datasets = Mock(get_datasets)
+    stub_get_datasets.return_value = [ORMDataset()]
+
+    result = await paginate_datasets(crud_impl=stub_get_datasets)
 
     assert isinstance(result, list)
     assert isinstance(result[0], ORMDataset)
