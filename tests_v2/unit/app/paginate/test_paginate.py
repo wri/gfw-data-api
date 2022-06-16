224e53e2698198ead865538c1f6e56693a4beb9a
gtempus <gary.tempus.jr@gmail.com>
2022-06-16 17:34:09 -0400

    :sparkles: feat(paginate): Introduce `meta` section for paginated results
    I don't care, at all, what my collaborator is doing for this first set of `meta` tests. Therefore, they are declared as `dummy` test doubles.
    
    GTC-1865

--- tests_v2/unit/app/paginate/test_paginate.py
+++ tests_v2/unit/app/paginate/test_paginate.py
@@ -9,7 +9,7 @@ from app.paginate.paginate import paginate_datasets
 
 @pytest.mark.asyncio
 async def test_legacy_no_pagination_happens_for_default_values():
-    """This is for legacy compatibility"""
+    """This is for legacy compatibility."""
     spy_get_datasets = Mock(get_datasets)
 
     await paginate_datasets(crud_impl=spy_get_datasets)
@@ -19,11 +19,29 @@ async def test_legacy_no_pagination_happens_for_default_values():
 
 @pytest.mark.asyncio
 async def test_legacy_datasets_collection_is_returned_when_no_arguments_are_given():
-    """This is for legacy compatibility"""
+    """This is for legacy compatibility."""
     stub_get_datasets = Mock(get_datasets)
     stub_get_datasets.return_value = [ORMDataset()]
 
-    result = await paginate_datasets(crud_impl=stub_get_datasets)
+    data, _ = await paginate_datasets(crud_impl=stub_get_datasets)
 
-    assert isinstance(result, list)
-    assert isinstance(result[0], ORMDataset)
+    assert isinstance(data, list)
+    assert isinstance(data[0], ORMDataset)
+
+
+@pytest.mark.asyncio
+async def test_legacy_meta_section_is_none_when_no_arguments_are_given():
+    dummy_get_datasets = Mock(get_datasets)
+
+    _, meta = await paginate_datasets(crud_impl=dummy_get_datasets)
+
+    assert meta is None
+
+
+@pytest.mark.asyncio
+async def test_sending_page_number_returns_a_dataset_collection_with_a_meta_section():
+    dummy_get_datasets = Mock(get_datasets)
+
+    _, meta = await paginate_datasets(crud_impl=dummy_get_datasets, page=1)
+
+    assert isinstance(meta, dict)
