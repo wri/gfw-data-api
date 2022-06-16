bf894dfb03409e21eb66618a1e27006b8a4eb306
gtempus <gary.tempus.jr@gmail.com>
2022-06-16 17:34:09 -0400

    feat(paginate): Introduce `meta` section for paginated results
    I don't care, at all, what my collaborator is doing for this first set of `meta` tests. Therefore, they are declared as `dummy` test doubles.
    
    GTC-1865

--- app/paginate/paginate.py
+++ app/paginate/paginate.py
@@ -1,10 +1,16 @@
-from typing import List
+from typing import Dict, List, Optional, Tuple
 
 from app.crud.datasets import get_datasets
 from app.models.orm.datasets import Dataset as ORMDataset
 
 
-async def paginate_datasets(crud_impl=get_datasets,
-                            size: int = None,
-                            page: int = 0) -> List[ORMDataset]:
-    return await crud_impl(size, page)
+async def paginate_datasets(
+    crud_impl=get_datasets, size: int = None, page: int = 0
+) -> Tuple[List[ORMDataset], Optional[Dict]]:
+    data = await crud_impl(size, page)
+
+    meta: Optional[Dict[str, int]] = None
+    if size or page:
+        meta = {}
+
+    return data, meta
