d7c3617240880dc6d05a9927c7200dea12f0c2ea
gtempus <gary.tempus.jr@gmail.com>
2022-06-17 16:56:43 -0400

    feat(paginate): Add `PaginatedDatasetsReponse` to the datasets controller
    - This is just the first cut. I have to learn about formatting json properties from pydantic models.
    
    GTC-1865

--- app/models/pydantic/responses.py
+++ app/models/pydantic/responses.py
@@ -6,3 +6,9 @@ from .base import StrictBaseModel
 class Response(StrictBaseModel):
     data: Any
     status: str = "success"
+
+
+class PaginationMeta(StrictBaseModel):
+    size: int
+    total_items: int
+    total_pages: int
