5af75861d074f358f718757ca4fde867e952f78c
gtempus <gary.tempus.jr@gmail.com>
2022-06-28 17:28:52 -0400

    feat(paginate): Add query params to `/datasets` endpoint
    The `datasets` controller now calls `paginate` with arguments from the query parameters.
    
    GTC-1865

--- tests_v2/unit/app/routes/datasets/datasets/test_datasets_with_pagination.py
+++ tests_v2/unit/app/routes/datasets/datasets/test_datasets_with_pagination.py
@@ -7,9 +7,27 @@ from app.models.pydantic.datasets import PaginatedDatasetsResponse
 
 
 @pytest.mark.asyncio
-async def test_get_datasets_returns_datasets_response(
+async def test_adding_page_number_returns_paginated_datasets_response(
     async_client: AsyncClient, generic_dataset: Tuple[str, str]
 ) -> None:
 
     resp = await async_client.get("/datasets?page[number]=1")
+    assert PaginatedDatasetsResponse(**resp.json())
+
+
+@pytest.mark.asyncio
+async def test_adding_size_parameter_returns_paginated_datasets_response(
+    async_client: AsyncClient, generic_dataset: Tuple[str, str]
+) -> None:
+
+    resp = await async_client.get("/datasets?page[size]=10")
+    assert PaginatedDatasetsResponse(**resp.json())
+
+
+@pytest.mark.asyncio
+async def test_adding_both_page_and_size_parameter_returns_paginated_datasets_response(
+    async_client: AsyncClient, generic_dataset: Tuple[str, str]
+) -> None:
+
+    resp = await async_client.get("/datasets?page[number]=1&page[size]=10")
     assert PaginatedDatasetsResponse(**resp.json())
