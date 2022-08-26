03e3bf8306f934be2832d843887d8dc3686a4239
gtempus <gary.tempus.jr@gmail.com>
2022-08-26 16:13:49 -0400

    :memo: docs(paginate): Add API docs
    GTC-1865

--- app/models/pydantic/responses.py
+++ app/models/pydantic/responses.py
@@ -1,4 +1,6 @@
-from typing import Any
+from typing import Any, Optional
+
+from pydantic import Field
 
 from .base import StrictBaseModel
 
@@ -9,14 +11,42 @@ class Response(StrictBaseModel):
 
 
 class PaginationLinks(StrictBaseModel):
-    self: str
-    first: str
-    last: str
-    prev: str
-    next: str
+    self: str = Field(
+        ...,
+        title="Contains the URL for the current page",
+        example="https://data-api.globalforestwatch.org/datasets?page[number]=1&page[size]=25",
+    )
+    first: str = Field(
+        ...,
+        title="Contains the URL for the first page",
+        example="https://data-api.globalforestwatch.org/datasets?page[number]=1&page[size]=25",
+    )
+    last: str = Field(
+        ...,
+        title="Contains the URL for the last page",
+        example="https://data-api.globalforestwatch.org/datasets?page[number]=4&page[size]=25",
+    )
+    prev: Optional[str] = Field(
+        None, title="Contains the URL for the previous page", example=""
+    )
+    next: Optional[str] = Field(
+        None,
+        title="Contains the URL for the next page",
+        example="https://data-api.globalforestwatch.org/datasets?page[number]=2&page[size]=25",
+    )
 
 
 class PaginationMeta(StrictBaseModel):
-    size: int
-    total_items: int
-    total_pages: int
+    size: int = Field(
+        ...,
+        title="Reflects the value used in the page[size] query parameter (or the default size of 10 if not provided)",
+        example="25",
+    )
+    total_items: int = Field(
+        ...,
+        title="Contains the total number of pages, assuming the page size specified in the page[size] query parameter",
+        example="100",
+    )
+    total_pages: int = Field(
+        ..., title="Contains the total number of items", example="4"
+    )
