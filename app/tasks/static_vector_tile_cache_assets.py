c90284f34c2e86db4812c24f262034e2d9e33db0
Solomon Negusse <solomon.negusse@wri.org>
2022-09-26 13:49:13 -0500

    update attribute names to match schema; misc task updates

--- app/tasks/static_vector_tile_cache_assets.py
+++ app/tasks/static_vector_tile_cache_assets.py
@@ -3,7 +3,7 @@ import json
 from typing import Any, Dict, List
 from uuid import UUID
 
-from ..crud import assets
+from ..crud import assets, metadata
 from ..models.orm.assets import Asset as ORMAsset
 from ..models.pydantic.assets import AssetType
 from ..models.pydantic.change_log import ChangeLog
@@ -81,7 +81,7 @@ async def static_vector_tile_cache_asset(
         "-T",
         ndjson_uri,
         "-C",
-        ",".join([field["field_name"] for field in field_attributes]),
+        ",".join([field["name"] for field in field_attributes]),
     ]
 
     export_ndjson = GdalPythonExportJob(
@@ -179,8 +179,7 @@ async def _get_vector_tile_server(
     creation_options: StaticVectorTileCacheCreationOptions,
 ) -> Dict[str, Any]:
 
-    asset: ORMAsset = await assets.get_asset(asset_id)
-    metadata: Dict[str, Any] = asset.metadata
+    version_metadata = await metadata.get_version_metadata(dataset, version)
 
     resolution = 78271.51696401172
     scale = 295829355.45453244
@@ -197,8 +196,8 @@ async def _get_vector_tile_server(
 
     response = {
         "currentVersion": 10.7,
-        "name": metadata["title"],
-        "copyrightText": metadata["citation"],
+        "name": version,
+        "copyrightText": version_metadata.citation,
         "capabilities": "TilesOnly",
         "type": "indexedVector",
         "defaultStyles": "resources/styles",
