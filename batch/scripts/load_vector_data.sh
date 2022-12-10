a593db58632db3a96f54af67adc22636433f0af3
Daniel Mannarino <daniel.mannarino@gmail.com>
2022-12-09 23:32:43 -0500

    Explicitly promote features to Multi, because ogr2ogr seems to be missing some

--- batch/scripts/load_vector_data.sh
+++ batch/scripts/load_vector_data.sh
@@ -30,6 +30,4 @@ ogr2ogr -f "PostgreSQL" PG:"password=$PGPASSWORD host=$PGHOST port=$PGPORT dbnam
      "$LOCAL_FILE" "$SRC_LAYER" \
      -nlt PROMOTE_TO_MULTI -nln "$DATASET.$VERSION" \
      -t_srs EPSG:4326 --config PG_USE_COPY YES \
-     --config OGR_DEBUG_ORGANIZE_POLYGONS YES \
-     --config CPL_DEBUG ON \
      -update -append -makevalid
