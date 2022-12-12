00078f3daedd3291480327dd3c7d4f2d724bd174
Gary Tempus Jr <gary.tempus@wri.org>
2022-12-12 18:46:03 -0500

    :white_check_mark: test: Move setup into fixtures
    Also, create a new test that begins to actually assert collaborator interactions.

--- tests_v2/unit/app/tasks/raster_tile_cache_assets/test_raster_tile_cache_assets.py
+++ tests_v2/unit/app/tasks/raster_tile_cache_assets/test_raster_tile_cache_assets.py
@@ -4,6 +4,7 @@ from uuid import UUID
 
 import pytest
 
+from app.models.enum.change_log import ChangeLogStatus
 from app.models.orm.assets import Asset as ORMAsset
 from app.models.pydantic.change_log import ChangeLog
 from app.models.pydantic.jobs import Job
@@ -11,26 +12,20 @@ from app.tasks.raster_tile_cache_assets import raster_tile_cache_asset
 from app.tasks.raster_tile_cache_assets.symbology import SymbologyInfo
 
 
-@patch("app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.execute")
-@patch(
-    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.symbology_constructor"
-)
-@patch(
-    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.reproject_to_web_mercator"
-)
-@patch("app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.get_asset")
-@pytest.mark.asyncio
-async def test_it_does_something(
-    get_asset_dummy, web_mercator_dummy, symbology_constructor_dummy, execute_mock
-):
-    get_asset_dummy.return_value = ORMAsset(
+@pytest.fixture()
+def source_asset():
+    return ORMAsset(
         creation_options={
             "pixel_meaning": "test pixels",
             "data_type": "boolean",
             "grid": "1/4000",
         }
     )
-    web_mercator_dummy.return_value = (
+
+
+@pytest.fixture()
+def reprojection():
+    return (
         Job(
             dataset="test_dataset",
             job_name="job",
@@ -45,12 +40,52 @@ async def test_it_does_something(
         "https://some/asset/uri",
     )
 
-    symbology_constructor_dummy.__getitem__.return_value = SymbologyInfo(
-        8, 1, AsyncMock(return_value=([], "myjob"))
-    )
-    execute_mock.return_value = ChangeLog(
+
+@pytest.fixture()
+def symbology_info():
+    return SymbologyInfo(8, 1, AsyncMock(return_value=([], "myjob")))
+
+
+@pytest.fixture()
+def change_log():
+    return ChangeLog(
         date_time=datetime(2022, 12, 6), status="success", message="All done!"
     )
+
+
+@patch(
+    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.execute", autospec=True
+)
+@patch(
+    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.symbology_constructor",
+    autospec=True,
+)
+@patch(
+    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.reproject_to_web_mercator",
+    autospec=True,
+)
+@patch(
+    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.get_asset",
+    autospec=True,
+)
+@pytest.mark.asyncio
+async def test_exploratory_test_runs_without_error(
+    get_asset_dummy,
+    web_mercator_dummy,
+    symbology_constructor_dummy,
+    execute_dummy,
+    source_asset,
+    reprojection,
+    symbology_info,
+    change_log,
+):
+    """Goal of this test is to determine the minimum amount of patching we need
+    to do to get the function to run as much side-effect free code as
+    possible."""
+    get_asset_dummy.return_value = source_asset
+    symbology_constructor_dummy.__getitem__.return_value = symbology_info
+    web_mercator_dummy.return_value = reprojection
+    execute_dummy.return_value = change_log
 
     TILE_CACHE_ASSET_UUID = UUID("e0a2dc44-aee1-4f71-963b-70a85869685d")
     SOURCE_ASSET_UUID = UUID("a8230040-23ad-4def-aca3-a292b6161557")
@@ -58,11 +93,11 @@ async def test_it_does_something(
     CREATION_OPTIONS = {
         "creation_options": {
             "min_zoom": 0,
-            "max_zoom": 0,
+            "max_zoom": 0,  # make sure to test this at least with 1
             "max_static_zoom": 0,
             "implementation": "default",
             "symbology": {
-                "type": "date_conf_intensity",
+                "type": "date_conf_intensity",  # try no_symbology
             },
             "resampling": "nearest",
             "source_asset_id": SOURCE_ASSET_UUID,
@@ -73,4 +108,61 @@ async def test_it_does_something(
         "test_dataset", "2022", TILE_CACHE_ASSET_UUID, CREATION_OPTIONS
     )
 
-    assert result.message == "All done!"
+    assert result.status == ChangeLogStatus.success
+
+
+@patch(
+    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.execute", autospec=True
+)
+@patch(
+    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.symbology_constructor",
+    autospec=True,
+)
+@patch(
+    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.reproject_to_web_mercator",
+    autospec=True,
+)
+@patch(
+    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.get_asset",
+    autospec=True,
+)
+@pytest.mark.asyncio
+async def test_source_asset_is_retrieved_by_uuid(
+    get_asset_mock,
+    web_mercator_dummy,
+    symbology_constructor_dummy,
+    execute_dummy,
+    source_asset,
+    reprojection,
+    symbology_info,
+    change_log,
+):
+    """Goal of this test is to determine the minimum amount of patching we need
+    to do to get the function to run as much side-effect free code as
+    possible."""
+    get_asset_mock.return_value = source_asset
+    symbology_constructor_dummy.__getitem__.return_value = symbology_info
+    web_mercator_dummy.return_value = reprojection
+    execute_dummy.return_value = change_log
+
+    TILE_CACHE_ASSET_UUID = UUID("e0a2dc44-aee1-4f71-963b-70a85869685d")
+    SOURCE_ASSET_UUID = UUID("a8230040-23ad-4def-aca3-a292b6161557")
+    CREATION_OPTIONS = {
+        "creation_options": {
+            "min_zoom": 0,
+            "max_zoom": 0,  # make sure to test this at least with 1
+            "max_static_zoom": 0,
+            "implementation": "default",
+            "symbology": {
+                "type": "date_conf_intensity",  # try no_symbology
+            },
+            "resampling": "nearest",
+            "source_asset_id": SOURCE_ASSET_UUID,
+        }
+    }
+
+    await raster_tile_cache_asset(
+        "test_dataset", "2022", TILE_CACHE_ASSET_UUID, CREATION_OPTIONS
+    )
+
+    get_asset_mock.assert_called_with(SOURCE_ASSET_UUID)
