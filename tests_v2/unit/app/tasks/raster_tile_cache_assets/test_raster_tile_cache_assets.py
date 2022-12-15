b835b2a00562fdcb29c58b1a783929aed3bbed50
Gary Tempus Jr <gary.tempus@wri.org>
2022-12-14 19:33:13 -0500

    :white_check_mark: test: Use `call_args_list` with mocks
    I definitely think this is a better form than what I was doing previously with `ANY`. The diffs are MUCH nicer.
    
    https://stackoverflow.com/questions/21611559/assert-that-a-method-was-called-with-one-argument-out-of-several/56765027#56765027

--- tests_v2/unit/app/tasks/raster_tile_cache_assets/test_raster_tile_cache_assets.py
+++ tests_v2/unit/app/tasks/raster_tile_cache_assets/test_raster_tile_cache_assets.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from unittest.mock import ANY, AsyncMock, patch
+from unittest.mock import AsyncMock, patch
 from uuid import UUID
 
 import pytest
@@ -8,7 +8,6 @@ from app.models.enum.change_log import ChangeLogStatus
 from app.models.orm.assets import Asset as ORMAsset
 from app.models.pydantic.change_log import ChangeLog
 from app.models.pydantic.jobs import Job
-from app.settings.globals import PIXETL_DEFAULT_RESAMPLING
 from app.tasks.raster_tile_cache_assets import raster_tile_cache_asset
 from app.tasks.raster_tile_cache_assets.symbology import SymbologyInfo
 
@@ -223,17 +222,8 @@ async def test_reproject_to_web_mercator_is_called_with_dataset_and_version(
         "test_dataset", "2022", TILE_CACHE_ASSET_UUID, CREATION_OPTIONS
     )
 
-    web_mercator_mock.assert_called_with(
-        dataset="test_dataset",
-        version="2022",
-        source_creation_options=ANY,
-        zoom_level=ANY,
-        max_zoom=ANY,
-        parents=ANY,
-        max_zoom_resampling=ANY,
-        max_zoom_calc=ANY,
-        use_resampler=ANY,
-    )
+    args, _ = web_mercator_mock.call_args_list[-1]
+    assert args[:2] == ("test_dataset", "2022"), "`dataset` and `version` do not match"
 
 
 @patch(
@@ -290,17 +280,11 @@ async def test_reproject_to_web_mercator_is_called_same_max_zoom_level_as_passed
         "test_dataset", "2022", TILE_CACHE_ASSET_UUID, CREATION_OPTIONS
     )
 
-    web_mercator_mock.assert_called_with(
-        dataset=ANY,
-        version=ANY,
-        source_creation_options=ANY,
-        zoom_level=ANY,
-        max_zoom=0,
-        parents=ANY,
-        max_zoom_resampling=ANY,
-        max_zoom_calc=ANY,
-        use_resampler=ANY,
-    )
+    args, _ = web_mercator_mock.call_args_list[-1]
+    assert args[3:5] == (
+        0,
+        0,
+    ), "`zoom_level` and `max_zoom` values were not as expected"
 
 
 @patch(
@@ -319,7 +303,7 @@ async def test_reproject_to_web_mercator_is_called_same_max_zoom_level_as_passed
     autospec=True,
 )
 @pytest.mark.asyncio
-async def test_reproject_to_web_mercator_is_called_with_default_pixetl_resampling(
+async def test_reproject_to_web_mercator_is_called_with_resampling_kwargs(
     get_asset_dummy,
     web_mercator_mock,
     symbology_constructor_dummy,
@@ -357,148 +341,9 @@ async def test_reproject_to_web_mercator_is_called_with_default_pixetl_resamplin
         "test_dataset", "2022", TILE_CACHE_ASSET_UUID, CREATION_OPTIONS
     )
 
-    web_mercator_mock.assert_called_with(
-        dataset=ANY,
-        version=ANY,
-        source_creation_options=ANY,
-        zoom_level=ANY,
-        max_zoom=ANY,
-        parents=ANY,
-        max_zoom_resampling=PIXETL_DEFAULT_RESAMPLING,
-        max_zoom_calc=ANY,
-        use_resampler=ANY,
-    )
-
-
-@patch(
-    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.execute", autospec=True
-)
-@patch(
-    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.symbology_constructor",
-    autospec=True,
-)
-@patch(
-    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.reproject_to_web_mercator",
-    autospec=True,
-)
-@patch(
-    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.get_asset",
-    autospec=True,
-)
-@pytest.mark.asyncio
-async def test_reproject_to_web_mercator_is_called_with_a_max_zoom_calc_of_none(
-    get_asset_dummy,
-    web_mercator_mock,
-    symbology_constructor_dummy,
-    execute_dummy,
-    source_asset,
-    reprojection,
-    symbology_info,
-    change_log,
-):
-    """Goal of this test is to determine the minimum amount of patching we need
-    to do to get the function to run as much side-effect free code as
-    possible."""
-    get_asset_dummy.return_value = source_asset
-    symbology_constructor_dummy.__getitem__.return_value = symbology_info
-    web_mercator_mock.return_value = reprojection
-    execute_dummy.return_value = change_log
-
-    TILE_CACHE_ASSET_UUID = UUID("e0a2dc44-aee1-4f71-963b-70a85869685d")
-    SOURCE_ASSET_UUID = UUID("a8230040-23ad-4def-aca3-a292b6161557")
-    CREATION_OPTIONS = {
-        "creation_options": {
-            "min_zoom": 0,
-            "max_zoom": 0,  # make sure to test this at least with 1
-            "max_static_zoom": 0,
-            "implementation": "default",
-            "symbology": {
-                "type": "date_conf_intensity",  # try no_symbology
-            },
-            "resampling": "nearest",
-            "source_asset_id": SOURCE_ASSET_UUID,
-        }
-    }
-
-    await raster_tile_cache_asset(
-        "test_dataset", "2022", TILE_CACHE_ASSET_UUID, CREATION_OPTIONS
-    )
-
-    web_mercator_mock.assert_called_with(
-        dataset=ANY,
-        version=ANY,
-        source_creation_options=ANY,
-        zoom_level=ANY,
-        max_zoom=ANY,
-        parents=ANY,
-        max_zoom_resampling=ANY,
-        max_zoom_calc=None,
-        use_resampler=ANY,
-    )
-
-
-@patch(
-    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.execute", autospec=True
-)
-@patch(
-    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.symbology_constructor",
-    autospec=True,
-)
-@patch(
-    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.reproject_to_web_mercator",
-    autospec=True,
-)
-@patch(
-    "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.get_asset",
-    autospec=True,
-)
-@pytest.mark.asyncio
-async def test_reproject_to_web_mercator_does_not_use_resampler(
-    get_asset_dummy,
-    web_mercator_mock,
-    symbology_constructor_dummy,
-    execute_dummy,
-    source_asset,
-    reprojection,
-    symbology_info,
-    change_log,
-):
-    """Goal of this test is to determine the minimum amount of patching we need
-    to do to get the function to run as much side-effect free code as
-    possible."""
-    get_asset_dummy.return_value = source_asset
-    symbology_constructor_dummy.__getitem__.return_value = symbology_info
-    web_mercator_mock.return_value = reprojection
-    execute_dummy.return_value = change_log
-
-    TILE_CACHE_ASSET_UUID = UUID("e0a2dc44-aee1-4f71-963b-70a85869685d")
-    SOURCE_ASSET_UUID = UUID("a8230040-23ad-4def-aca3-a292b6161557")
-    CREATION_OPTIONS = {
-        "creation_options": {
-            "min_zoom": 0,
-            "max_zoom": 0,  # make sure to test this at least with 1
-            "max_static_zoom": 0,
-            "implementation": "default",
-            "symbology": {
-                "type": "date_conf_intensity",  # try no_symbology
-            },
-            "resampling": "nearest",
-            "source_asset_id": SOURCE_ASSET_UUID,
-        }
-    }
-
-    await raster_tile_cache_asset(
-        "test_dataset", "2022", TILE_CACHE_ASSET_UUID, CREATION_OPTIONS
-    )
-
-    web_mercator_mock.assert_called_with(
-        dataset=ANY,
-        version=ANY,
-        source_creation_options=ANY,
-        zoom_level=ANY,
-        max_zoom=ANY,
-        parents=ANY,
-        max_zoom_resampling=ANY,
-        max_zoom_calc=ANY,
-        use_resampler=True,
-    )
+    _, kwargs = web_mercator_mock.call_args_list[-1]
+    assert kwargs == {
+        "max_zoom_resampling": "nearest",
+        "max_zoom_calc": None,
+        "use_resampler": True,
+    }, "`Resampling` arguments do not match"
