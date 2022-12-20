fa88c9e81f2f96ae6970fc499c5883e342310af7
Gary Tempus Jr <gary.tempus@wri.org>
2022-12-20 10:03:05 -0500

    :white_check_mark: test: Add integration test for task execution

--- tests_v2/unit/app/tasks/raster_tile_cache_assets/test_raster_tile_cache_assets.py
+++ tests_v2/unit/app/tasks/raster_tile_cache_assets/test_raster_tile_cache_assets.py
@@ -70,6 +70,21 @@ def raster_tile_set_source_creation_options():
 
 
 @pytest.fixture()
+def tile_cache_job():
+    return Job(
+        dataset="test_dataset",
+        job_name="tile_cache_job",
+        job_queue="tile_cache_job_queue",
+        job_definition="tile cache job",
+        command=["doit"],
+        vcpus=1,
+        memory=64,
+        attempts=1,
+        attempt_duration_seconds=1,
+    )
+
+
+@pytest.fixture()
 def symbology_job():
     return Job(
         dataset="test_dataset",
@@ -88,8 +103,8 @@ def symbology_job():
 def job():
     return Job(
         dataset="test_dataset",
-        job_name="job",
-        job_queue="job queue",
+        job_name="reprojection_job",
+        job_queue="reprojection_job_queue",
         job_definition="source reprojection job",
         command=["doit"],
         vcpus=1,
@@ -1453,4 +1468,56 @@ class TestCreateTileCacheIntegration:
 
 
 class TestTaskExecutionIntegration:
-    pass
+    @patch(
+        "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.execute",
+        autospec=True,
+    )
+    @patch(
+        "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.create_tile_cache",
+        autospec=True,
+    )
+    @patch(
+        "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.callback_constructor",
+        autospec=True,
+    )
+    @patch(
+        "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.symbology_constructor",
+        autospec=True,
+    )
+    @patch(
+        "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.reproject_to_web_mercator",
+        autospec=True,
+    )
+    @patch(
+        "app.tasks.raster_tile_cache_assets.raster_tile_cache_assets.get_asset",
+        autospec=True,
+    )
+    @pytest.mark.asyncio
+    async def test_is_called_with_complete_job_list_for_execution(
+        self,
+        get_asset_dummy,
+        web_mercator_dummy,
+        symbology_constructor_dummy,
+        callback_constructor_dummy,
+        create_tile_cache_dummy,
+        execute_mock,
+        tile_cache_asset_uuid,
+        creation_options_dict,
+        source_asset,
+        reprojection,
+        symbology_info,
+        tile_cache_job,
+        change_log,
+    ):
+        get_asset_dummy.return_value = source_asset
+        symbology_constructor_dummy.__getitem__.return_value = symbology_info
+        web_mercator_dummy.return_value = reprojection
+        create_tile_cache_dummy.return_value = tile_cache_job
+        execute_mock.return_value = change_log
+        await raster_tile_cache_asset(
+            "test_dataset", "2022", tile_cache_asset_uuid, creation_options_dict
+        )
+
+        args, _ = execute_mock.call_args_list[-1]
+        result = [job.job_name for job in args[0]]
+        assert result == ["reprojection_job", "symbology_job", "tile_cache_job"]
