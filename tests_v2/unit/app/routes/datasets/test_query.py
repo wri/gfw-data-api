457255d105a14fa9d03eb762e346e620101d7a5b
Daniel Mannarino <daniel.mannarino@gmail.com>
2023-02-09 12:16:49 -0500

    Add some tests for _get_data_environment

--- tests_v2/unit/app/routes/datasets/test_query.py
+++ tests_v2/unit/app/routes/datasets/test_query.py
@@ -1,9 +1,25 @@
+from typing import Tuple
+from unittest.mock import Mock
+
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 from httpx import AsyncClient
 
-from app.routes.datasets import queries
-from tests_v2.utils import invoke_lambda_mocked
+from app.models.enum.pixetl import Grid
+from app.models.pydantic.raster_analysis import DerivedLayer, SourceLayer
+from app.routes.datasets import queries, versions
+from app.routes.datasets.queries import _get_data_environment
+from app.tasks import batch, delete_assets
+from app.tasks.raster_tile_set_assets import raster_tile_set_assets
+from tests_v2.fixtures.creation_options.versions import RASTER_CREATION_OPTIONS
+from tests_v2.utils import (
+    BatchJobMock,
+    dict_function_closure,
+    get_extent_mocked,
+    int_function_closure,
+    invoke_lambda_mocked,
+    void_coroutine,
+)
 
 
 @pytest.mark.skip("Temporarily skip until we require API keys")
@@ -378,4 +394,207 @@ async def test_query_vector_asset_disallowed_10(
         "Bad request. function doesnotexist() does not exist\n"
         "HINT:  No function matches the given name and argument types. "
         "You might need to add explicit type casts."
+    )
+
+
+@pytest.mark.asyncio
+async def test__get_data_environment_helpers_called_dateconf(
+    async_client: AsyncClient,
+    generic_dataset: Tuple[str, str],
+    monkeypatch: MonkeyPatch,
+):
+    dataset_name, _ = generic_dataset
+    version_name: str = "v1"
+
+    # Patch all functions which reach out to external services
+    # Basically we're leaving out everything but the DB entries being created
+    batch_job_mock = BatchJobMock()
+    monkeypatch.setattr(versions, "_verify_source_file_access", void_coroutine)
+    monkeypatch.setattr(batch, "submit_batch_job", batch_job_mock.submit_batch_job)
+    monkeypatch.setattr(delete_assets, "delete_s3_objects", int_function_closure(1))
+    monkeypatch.setattr(raster_tile_set_assets, "get_extent", get_extent_mocked)
+    monkeypatch.setattr(
+        delete_assets, "flush_cloudfront_cache", dict_function_closure({})
+    )
+
+    pixel_meaning: str = "date_conf"
+    no_data_value = 0
+
+    # Create version
+    await async_client.put(
+        f"/dataset/{dataset_name}/{version_name}",
+        json={
+            "creation_options": {
+                **RASTER_CREATION_OPTIONS,
+                **{"pixel_meaning": pixel_meaning, "no_data": no_data_value},
+            }
+        },
+    )
+
+    await async_client.patch(
+        f"/dataset/{dataset_name}/{version_name}", json={"is_latest": True}
+    )
+
+    grid: Grid = Grid(RASTER_CREATION_OPTIONS["grid"])
+
+    _get_source_layer_mock = Mock(
+        queries._get_source_layer,
+        return_value=SourceLayer(
+            name="foo", source_uri="some_source_uri", grid=grid, no_data=no_data_value
+        ),
+    )
+    monkeypatch.setattr(queries, "_get_source_layer", _get_source_layer_mock)
+    _get_date_conf_derived_layers_mock = Mock(
+        queries._get_date_conf_derived_layers, return_value=[]
+    )
+    monkeypatch.setattr(
+        queries, "_get_date_conf_derived_layers", _get_date_conf_derived_layers_mock
+    )
+
+    _ = await _get_data_environment(grid)
+
+    assert _get_date_conf_derived_layers_mock.call_args.args == (
+        f"{dataset_name}__{pixel_meaning}",
+        no_data_value,
+    )
+
+    assert _get_source_layer_mock.call_args.args == (
+        f"s3://gfw-data-lake-test/{dataset_name}/v1/raster/epsg-4326/{grid.value}/{pixel_meaning}/geotiff/"
+        + "{tile_id}.tif",
+        f"{dataset_name}__{pixel_meaning}",
+        grid,
+        no_data_value,
+        None,
+    )
+
+
+@pytest.mark.asyncio
+async def test__get_data_environment_helpers_called_area_density(
+    async_client: AsyncClient,
+    generic_dataset: Tuple[str, str],
+    monkeypatch: MonkeyPatch,
+):
+    dataset_name, _ = generic_dataset
+    version_name: str = "v1"
+
+    # Patch all functions which reach out to external services
+    # Basically we're leaving out everything but the DB entries being created
+    batch_job_mock = BatchJobMock()
+    monkeypatch.setattr(versions, "_verify_source_file_access", void_coroutine)
+    monkeypatch.setattr(batch, "submit_batch_job", batch_job_mock.submit_batch_job)
+    monkeypatch.setattr(delete_assets, "delete_s3_objects", int_function_closure(1))
+    monkeypatch.setattr(raster_tile_set_assets, "get_extent", get_extent_mocked)
+    monkeypatch.setattr(
+        delete_assets, "flush_cloudfront_cache", dict_function_closure({})
+    )
+
+    pixel_meaning: str = "hamsters_ha-1"
+    no_data_value = None
+
+    # Create version
+    await async_client.put(
+        f"/dataset/{dataset_name}/{version_name}",
+        json={
+            "creation_options": {
+                **RASTER_CREATION_OPTIONS,
+                **{"pixel_meaning": pixel_meaning, "no_data": no_data_value},
+            }
+        },
+    )
+
+    await async_client.patch(
+        f"/dataset/{dataset_name}/{version_name}", json={"is_latest": True}
+    )
+
+    grid: Grid = Grid(RASTER_CREATION_OPTIONS["grid"])
+
+    _get_source_layer_mock = Mock(
+        queries._get_source_layer,
+        return_value=SourceLayer(
+            name="foo", source_uri="some_source_uri", grid=grid, no_data=no_data_value
+        ),
+    )
+    monkeypatch.setattr(queries, "_get_source_layer", _get_source_layer_mock)
+
+    _get_area_density_layer_mock = Mock(
+        queries._get_area_density_layer, return_value=Mock(DerivedLayer, autospec=True)
+    )
+    monkeypatch.setattr(
+        queries, "_get_area_density_layer", _get_area_density_layer_mock
+    )
+
+    _ = await _get_data_environment(grid)
+
+    assert _get_area_density_layer_mock.call_args.args == (
+        f"{dataset_name}__{pixel_meaning}",
+        no_data_value,
+    )
+
+    assert _get_source_layer_mock.call_args.args == (
+        f"s3://gfw-data-lake-test/{dataset_name}/v1/raster/epsg-4326/{grid.value}/{pixel_meaning}/geotiff/"
+        + "{tile_id}.tif",
+        f"{dataset_name}__{pixel_meaning}",
+        grid,
+        no_data_value,
+        None,
+    )
+
+
+@pytest.mark.asyncio
+async def test__get_data_environment_helper_called(
+    async_client: AsyncClient,
+    generic_dataset: Tuple[str, str],
+    monkeypatch: MonkeyPatch,
+):
+    dataset_name, _ = generic_dataset
+    version_name: str = "v1"
+
+    # Patch all functions which reach out to external services
+    # Basically we're leaving out everything but the DB entries being created
+    batch_job_mock = BatchJobMock()
+    monkeypatch.setattr(versions, "_verify_source_file_access", void_coroutine)
+    monkeypatch.setattr(batch, "submit_batch_job", batch_job_mock.submit_batch_job)
+    monkeypatch.setattr(delete_assets, "delete_s3_objects", int_function_closure(1))
+    monkeypatch.setattr(raster_tile_set_assets, "get_extent", get_extent_mocked)
+    monkeypatch.setattr(
+        delete_assets, "flush_cloudfront_cache", dict_function_closure({})
+    )
+
+    pixel_meaning: str = "foo"
+    no_data_value = 255
+
+    # Create version
+    await async_client.put(
+        f"/dataset/{dataset_name}/{version_name}",
+        json={
+            "creation_options": {
+                **RASTER_CREATION_OPTIONS,
+                **{"pixel_meaning": pixel_meaning, "no_data": no_data_value},
+            }
+        },
+    )
+
+    await async_client.patch(
+        f"/dataset/{dataset_name}/{version_name}", json={"is_latest": True}
+    )
+
+    grid: Grid = Grid(RASTER_CREATION_OPTIONS["grid"])
+
+    _get_source_layer_mock = Mock(
+        queries._get_source_layer,
+        return_value=SourceLayer(
+            name="foo", source_uri="some_source_uri", grid=grid, no_data=no_data_value
+        ),
+    )
+    monkeypatch.setattr(queries, "_get_source_layer", _get_source_layer_mock)
+
+    _ = await _get_data_environment(grid)
+
+    assert _get_source_layer_mock.call_args.args == (
+        f"s3://gfw-data-lake-test/{dataset_name}/v1/raster/epsg-4326/{grid.value}/{pixel_meaning}/geotiff/"
+        + "{tile_id}.tif",
+        f"{dataset_name}__{pixel_meaning}",
+        grid,
+        no_data_value,
+        None,
     )
