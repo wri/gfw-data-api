c2500adbe6ac5498995170809dc495219b233ba0
Daniel Mannarino <daniel.mannarino@gmail.com>
2023-05-03 16:17:36 -0400

    Clarify/format explanatory comment; fix indentation of code block

--- batch/scripts/create_tabular_schema.sh
+++ batch/scripts/create_tabular_schema.sh
@@ -36,24 +36,24 @@ fi
 # csvsql sets the quotes for schema and table wrong. It is safer to set the schema separately
 sed -i "1s/^/SET SCHEMA '$DATASET';\n/" create_table.sql
 
-# update field types
-# This expects a JSON List like this '[{"name":"name1", "data_type":"type1"},{"name":"name2", "data_type":"type2"}]'
-# It will export the different key value pairs as ENV variables so that we can reference them within the for loop
-# We will then update rows within our create_table.sql file using the new field type
+# If the user has specified a field map, override csvsql's inferred field
+# types with those specified. It is expected that $FIELD_MAP contains (after
+# sourcing get_arguments.sh above) a JSON List of the form
+# '[{"name":"name1", "data_type":"type1"},{"name":"name2", "data_type":"type2"}]'
 # https://starkandwayne.com/blog/bash-for-loop-over-json-array-using-jq/
 if [[ -n "${FIELD_MAP}" ]]; then
   for row in $(echo "${FIELD_MAP}" | jq -r '.[] | @base64'); do
-      _jq() {
-       echo "${row}" | base64 --decode | jq -r "${1}"
-      }
-     FIELD_NAME=$(_jq '.name')
-     FIELD_TYPE=$(_jq '.data_type')
+    _jq() {
+      echo "${row}" | base64 --decode | jq -r "${1}"
+    }
+    FIELD_NAME=$(_jq '.name')
+    FIELD_TYPE=$(_jq '.data_type')
 
-     # field names might be in double quotes
-     # make sure there is no comma after the last field
-     sed -i "s/^\t${FIELD_NAME} .*$/\t${FIELD_NAME} ${FIELD_TYPE},/" create_table.sql
-     sed -i "s/^\t\"${FIELD_NAME}\" .*$/\t\"${FIELD_NAME}\" ${FIELD_TYPE},/" create_table.sql
-     sed -i 'x; ${s/,//;p;x}; 1d' create_table.sql
+    # field names might be in double quotes
+    # make sure there is no comma after the last field
+    sed -i "s/^\t${FIELD_NAME} .*$/\t${FIELD_NAME} ${FIELD_TYPE},/" create_table.sql
+    sed -i "s/^\t\"${FIELD_NAME}\" .*$/\t\"${FIELD_NAME}\" ${FIELD_TYPE},/" create_table.sql
+    sed -i 'x; ${s/,//;p;x}; 1d' create_table.sql
   done
 fi
 
