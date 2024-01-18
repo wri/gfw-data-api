998ea66e669510096a9e516c87d8e7866ee3b630
Daniel Mannarino <daniel.mannarino@gmail.com>
2024-01-17 21:09:24 -0500

    Refactor get_gs_files_async to be easier to test

--- app/utils/google.py
+++ app/utils/google.py
@@ -53,6 +53,26 @@ async def set_google_application_credentials() -> bool:
     expected_exception=(RuntimeError,),
     on_exception=set_google_application_credentials,
 )
+async def list_gs_objects(bucket: str, prefix: str) -> aiogoogle.models.Response:
+    sam = aiogoogle.auth.ServiceAccountManager()
+
+    await sam.detect_default_creds_source()
+
+    creds = sam.creds
+    creds["scopes"] = [
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/cloud-platform.read-only",
+    ]
+
+    async with aiogoogle.Aiogoogle(service_account_creds=creds) as aiogoogle_api:
+        storage = await aiogoogle_api.discover('storage', 'v1')
+        results = await aiogoogle_api.as_service_account(
+            storage.objects.list(bucket=bucket, prefix=prefix),
+            full_res=True
+        )
+    return results
+
+
 async def get_gs_files_async(
     bucket: str,
     prefix: str,
@@ -62,27 +82,15 @@ async def get_gs_files_async(
 ) -> List[str]:
     """Get all matching files in GCS."""
 
+    # NOTE: We can limit the number of results per page in list_gs_objects
+    # but not the total results returned from GCS. So I'm afraid the limit
+    # arg to this function is a lie :(
+
     matches: List[str] = list()
     num_matches: int = 0
 
-    sam = aiogoogle.auth.ServiceAccountManager()
-    await sam.detect_default_creds_source()
-    creds = sam.creds
-    creds["scopes"] = [
-        "https://www.googleapis.com/auth/devstorage.read_only",
-        "https://www.googleapis.com/auth/cloud-platform.read-only",
-    ]
-
-    async with aiogoogle.Aiogoogle(service_account_creds=creds) as aiogoogle_api:
-        storage = await aiogoogle_api.discover('storage', 'v1')
-        full_res = await aiogoogle_api.as_service_account(
-            # NOTE: maxResults limits the number of results per page, but not
-            # the total results returned. So I'm afraid the limit arg to this
-            # function is a lie :(
-            storage.objects.list(bucket=bucket, prefix=prefix),
-            full_res=True
-        )
-    async for page in full_res:
+    list_resp: aiogoogle.models.Response = await list_gs_objects(bucket, prefix)
+    async for page in list_resp:
         for blob in page["items"]:
             if not extensions or any(blob.name.endswith(ext) for ext in extensions):
                 matches.append(f"/vsigs/{bucket}/{blob['name']}")
