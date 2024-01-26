6db4b6592b237935cefc98e352c1a4296a195971
Daniel Mannarino <daniel.mannarino@gmail.com>
2024-01-26 01:50:17 -0500

    Switch back to official Google cloud pkg

--- app/utils/google.py
+++ app/utils/google.py
@@ -1,12 +1,12 @@
 import json
+from functools import lru_cache
 from typing import List, Optional, Sequence, Dict
 
-import aioboto3
-import aiogoogle
-from aiogoogle.auth.creds import ServiceAccountCreds
-from async_lru import alru_cache
+from google.cloud.storage import Client
+from google.oauth2 import service_account
 from limiter import Limiter
 
+from .aws import get_secret_client
 from ..settings.globals import AWS_GCS_KEY_SECRET_ARN, S3_ENTRYPOINT_URL, AWS_REGION
 
 
@@ -14,57 +14,50 @@ from ..settings.globals import AWS_GCS_KEY_SECRET_ARN, S3_ENTRYPOINT_URL, AWS_RE
 limit_api_calls = Limiter(rate=10, capacity=10, consume=1)
 
 
-@alru_cache(maxsize=1)
-async def get_gcs_service_account_key() -> Dict[str, str]:
-    session = aioboto3.Session()
-    async with session.client(
-        "secretsmanager", region_name=AWS_REGION, endpoint_url=S3_ENTRYPOINT_URL
-    ) as secrets_client:
-        response = await secrets_client.get_secret_value(SecretId=AWS_GCS_KEY_SECRET_ARN)
-        return json.loads(response["SecretString"])
+@lru_cache(maxsize=1)
+def get_gcs_service_account_auth_info() -> Dict[str, str]:
+    secret_client = get_secret_client()
+    response = secret_client.get_secret_value(SecretId=AWS_GCS_KEY_SECRET_ARN)
+    return json.loads(response["SecretString"])
 
 
-@limit_api_calls
-async def get_prefix_objects(bucket: str, prefix: str) -> List[str]:
+# @limit_api_calls
+def get_prefix_objects(bucket: str, prefix: str, limit: Optional[int] = None) -> List[str]:
     """Get ALL object names under a bucket and prefix in GCS."""
 
-    service_account_info = await get_gcs_service_account_key()
+    auth_info = get_gcs_service_account_auth_info()
+    scopes = [
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/cloud-platform.read-only",
+    ]
 
-    creds = ServiceAccountCreds(
-        scopes=[
-            "https://www.googleapis.com/auth/devstorage.read_only",
-            "https://www.googleapis.com/auth/cloud-platform.read-only",
-        ],
-        **service_account_info,
+    account_info = {
+        "scopes": scopes,
+        **auth_info
+    }
+
+    service_account_info = service_account.Credentials.from_service_account_info(
+        account_info
     )
+    client = Client(project=None, credentials=service_account_info)
 
-    async with aiogoogle.Aiogoogle(service_account_creds=creds) as aiogoogle_api:
-        storage = await aiogoogle_api.discover("storage", "v1")
-        response: aiogoogle.models.Response = await aiogoogle_api.as_service_account(
-            storage.objects.list(bucket=bucket, prefix=prefix), full_res=True
-        )
-    results = response.json.get("items", [])
-
-    return [blob["name"] for blob in results]
+    blobs = client.list_blobs(bucket, prefix=prefix, max_results=limit)
+    return [blob.name for blob in blobs]
 
 
-async def get_gs_files(
+def get_gs_files(
     bucket: str,
     prefix: str,
-    limit: Optional[int] = None,  # Ignored for this function! :(
+    limit: Optional[int] = None,
     exit_after_max: Optional[int] = None,
-    extensions: Sequence[str] = tuple(),
+    extensions: Sequence[str] = tuple()
 ) -> List[str]:
     """Get matching object names under a bucket and prefix in GCS."""
-
-    # NOTE: We can limit the number of results per page in list_gs_objects
-    # but not the total results returned from GCS. So I'm afraid the limit
-    # arg to this function is a lie :(
 
     matches: List[str] = list()
     num_matches: int = 0
 
-    for blob_name in await get_prefix_objects(bucket, prefix):
+    for blob_name in get_prefix_objects(bucket, prefix, limit):
         if not extensions or any(blob_name.endswith(ext) for ext in extensions):
             matches.append(f"/vsigs/{bucket}/{blob_name}")
             num_matches += 1
