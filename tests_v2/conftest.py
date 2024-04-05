c145ed1cff35af26b04a6e873639e34e2d786b98
Daniel Mannarino <daniel.mannarino@gmail.com>
2024-04-05 09:47:30 -0400

    Reluctantly duplicate async_client fixture for per-function version

--- tests_v2/conftest.py
+++ tests_v2/conftest.py
@@ -96,7 +96,25 @@ async def async_client(db, init_db) -> AsyncGenerator[AsyncClient, None]:
 @pytest_asyncio.fixture(scope="function")
 async def async_client_per_function(async_client) -> AsyncGenerator[AsyncClient, None]:
     """Async Test Client that's limited to per-function scope."""
-    yield async_client
+    from app.main import app
+
+    # mock authentication function to avoid having to reach out to RW API during tests
+    app.dependency_overrides[is_admin] = bool_function_closure(True, with_args=False)
+    app.dependency_overrides[is_service_account] = bool_function_closure(
+        True, with_args=False
+    )
+    app.dependency_overrides[get_user] = get_admin_mocked
+
+    async with AsyncClient(
+        app=app,
+        base_url="http://test",
+        trust_env=False,
+        headers={"Origin": "https://www.globalforestwatch.org"},
+    ) as client:
+        yield client
+
+    # Clean up
+    app.dependency_overrides = {}
 
 
 @pytest_asyncio.fixture
