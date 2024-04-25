d61f8ab4216925dfa7756786b98f5e0cb45eda71
Justin Terry <justin.terry@wri.org>
2024-04-25 12:33:45 -0700

    Add owner_id column to datasets

--- app/models/orm/datasets.py
+++ app/models/orm/datasets.py
@@ -5,4 +5,5 @@ class Dataset(Base):
     __tablename__ = "datasets"
     dataset = db.Column(db.String, primary_key=True)
     is_downloadable = db.Column(db.Boolean, nullable=False, default=True)
+    owner_id = db.Column(db.UUID, nullable=True)
     # metadata = db.Column(db.JSONB, default=dict())
