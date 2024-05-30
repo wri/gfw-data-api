9c66b7241cc95c77192d2e33bade8a8a1fc8fef1
Justin Terry <justin.terry@wri.org>
2024-05-29 17:05:56 -0700

    Add batch script to convert tile sets to COGs

---
+++ batch/scripts/cogify.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+# requires arguments
+# -d | --dataset
+# -v | --version
+# -s | --source
+# --target_bucket
+
+ME=$(basename "$0")
+. get_arguments.sh "$@"
+
+# download all GeoTiff files
+aws s3 cp --recursive --exclude "*" --include "*.tif" "${SRC}" .
+
+# get list of file names
+sources=()
+for source in *.tif; do
+  sources+=("$source")
+done
+
+# combine to one big COG
+gdalwarp "${sources[@]}" "${DATASET}_${VERSION}.tif" -of COG -co NUM_THREADS=ALL_CPUS
+
+# upload to data lake
+aws s3 cp "${DATASET}_${VERSION}.tif" "s3://${TARGET_BUCKET}/${DATASET}/${VERSION}/epsg-4326/cog/${DATASET}_${VERSION}.tif"
+
