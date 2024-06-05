146af38069b5403166e7e4a2bc0b852d70e2161d
Justin Terry <justin.terry@wri.org>
2024-06-05 13:10:27 -0700

    Break up gdalwarp COG command to multiple shorter commands

--- batch/scripts/cogify.sh
+++ batch/scripts/cogify.sh
@@ -16,8 +16,27 @@ set -x
 # download all GeoTiff files
 aws s3 cp --recursive --exclude "*" --include "*.tif" "${SRC}" .
 
-# combine to one big COG
-gdalwarp *.tif "${DATASET}_${VERSION}.tif" -r "${RESAMPLE}" -t_srs "${SRID}" -of COG -co BLOCKSIZE="${BLOCKSIZE}" -co NUM_THREADS=ALL_CPUS
+set GDAL_NUM_THREADS=ALL_CPUS
+
+# create VRT of input files so we can use gdal_translate
+if [ ! -f "merged.vrt" ]; then
+  gdalbuildvrt merged.vrt *.tif
+fi
+
+# merge all rasters into one huge raster using COG block size
+if [ ! -f "merged.tif" ]; then
+  gdal_translate -of GTiff -co BLOCKSIZE="${BLOCKSIZE}" -co COMPRESS=LZW -co NUM_THREADS=ALL_CPUS merged.vrt merged.tif
+fi
+
+# create overviews in raster
+if ! gdalinfo "merged.tif" | grep -q "Overviews"; then
+  gdaladdo merged.tif -r "${RESAMPLE}"
+fi
+
+# convert to COG using existing overviews, this adds some additional layout optimizations
+if [ ! -f "${DATASET}_${VERSION}.tif" ]; then
+  gdal_translate merged.tif "${DATASET}_${VERSION}.tif" -of COG -co BLOCKSIZE="${BLOCKSIZE}" -co NUM_THREADS=ALL_CPUS
+fi
 
 # upload to data lake
 aws s3 cp "${DATASET}_${VERSION}.tif" "s3://${TARGET_BUCKET}/${TARGET_PREFIX}"
