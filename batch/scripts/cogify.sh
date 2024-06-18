d8feb9482a7da6e3fedf7b79f005847ae77ff39e
Justin Terry <justin.terry@wri.org>
2024-06-18 16:27:57 -0700

    Create COG in WGS84

--- batch/scripts/cogify.sh
+++ batch/scripts/cogify.sh
@@ -19,24 +19,26 @@ set -x
 aws s3 cp --recursive --exclude "*" --include "*.tif" "${SRC}" .
 
 # create VRT of input files so we can use gdal_translate
-if [ ! -f "merged.vrt" ]; then
-  gdalbuildvrt merged.vrt *.tif
-fi
+#if [ ! -f "merged.vrt" ]; then
+#  gdalbuildvrt merged.vrt *.tif
+#fi
+#
+## merge all rasters into one huge raster using COG block size
+#if [ ! -f "merged.tif" ]; then
+#  gdal_translate -of GTiff -co TILED=YES -co BLOCKXSIZE="${BLOCK_SIZE}" -co BLOCKYSIZE="${BLOCK_SIZE}" -co COMPRESS=DEFLATE -co BIGTIFF=IF_SAFER -co NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 70% --config GDAL_NUM_THREADS ALL_CPUS merged.vrt merged.tif
+#fi
+#
+## create overviews in raster
+#if ! gdalinfo "merged.tif" | grep -q "Overviews"; then
+#  gdaladdo merged.tif -r "${RESAMPLE}" --config GDAL_NUM_THREADS ALL_CPUS --config GDAL_CACHEMAX 70%
+#fi
+#
+## convert to COG using existing overviews, this adds some additional layout optimizations
+#if [ ! -f "cog.tif" ]; then
+#  gdal_translate merged.tif cog.tif -of COG -co COMPRESS=DEFLATE -co BLOCKSIZE="${BLOCK_SIZE}" -co BIGTIFF=IF_SAFER -co NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 70% --config GDAL_NUM_THREADS ALL_CPUS
+#fi
 
-# merge all rasters into one huge raster using COG block size
-if [ ! -f "merged.tif" ]; then
-  gdal_translate -of GTiff -co TILED=YES -co BLOCKXSIZE="${BLOCK_SIZE}" -co BLOCKYSIZE="${BLOCK_SIZE}" -co COMPRESS=DEFLATE -co BIGTIFF=IF_SAFER -co NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 70% --config GDAL_NUM_THREADS ALL_CPUS merged.vrt merged.tif
-fi
-
-# create overviews in raster
-if ! gdalinfo "merged.tif" | grep -q "Overviews"; then
-  gdaladdo merged.tif -r "${RESAMPLE}" --config GDAL_NUM_THREADS ALL_CPUS --config GDAL_CACHEMAX 70%
-fi
-
-# convert to COG using existing overviews, this adds some additional layout optimizations
-if [ ! -f "cog.tif" ]; then
-  gdal_translate merged.tif cog.tif -of COG -co COMPRESS=DEFLATE -co BLOCKSIZE="${BLOCK_SIZE}" -co BIGTIFF=IF_SAFER -co NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 70% --config GDAL_NUM_THREADS ALL_CPUS
-fi
+gdalwarp -r "${RESAMPLE}" -t_srs epsg:3857 -of COG -co COMPRESS=DEFLATE -co BLOCKSIZE="${BLOCK_SIZE}" -co BIGTIFF=IF_SAFER -co NUM_THREADS=ALL_CPUS --config GDAL_CACHEMAX 70% --config GDAL_NUM_THREADS ALL_CPUS *.tif cog.tif
 
 # upload to data lake
 aws s3 cp cog.tif "${TARGET}"
