627421902dbfae4e82fa9f21d4258641afd2a99e
Daniel Mannarino <daniel.mannarino@gmail.com>
2024-08-12 14:21:40 -0400

    Un-silence curl command in report status script

--- batch/scripts/report_status.sh
+++ batch/scripts/report_status.sh
@@ -81,9 +81,7 @@ set -x
 echo "$(generate_payload)"
 
 CTYPE_HEADER="Content-Type:application/json"
-RESPONSE=$(curl -s -X PATCH -H "${AUTH_HEADER}" -H "${CTYPE_HEADER}" -d "$(generate_payload)" "${URL}")
-
-echo $RESPONSE
+curl -i -X PATCH -H "${AUTH_HEADER}" -H "${CTYPE_HEADER}" -d "$(generate_payload)" "${URL}"
 
 # Try to clean up free space for potential other batch jobs on the same node
 set +e
