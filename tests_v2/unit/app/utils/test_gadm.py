aa9cba41a9a5c2087594624ce2e6ade9b45bc481
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-04-28 00:20:19 -0400

    Add tests verifying current behavior

--- tests_v2/unit/app/utils/test_gadm.py
+++ tests_v2/unit/app/utils/test_gadm.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.utils.gadm import extract_level_id
+from app.utils.gadm import extract_level_id, fix_id_pattern
 
 
 @pytest.mark.asyncio
@@ -22,3 +22,39 @@ async def test_extract_level_gid() -> None:
     assert extract_level_id(0, match3) == "IDN"
     assert extract_level_id(1, match3) == "35"
     assert extract_level_id(2, match3) == "4"
+
+
+@pytest.mark.asyncio
+async def test_fix_id_pattern_unknown_gadms_pass_through() -> None:
+    orig_pattern = r"USA.5.10\__"
+    assert fix_id_pattern(2, orig_pattern, "gadm", "3.9") == orig_pattern
+
+
+@pytest.mark.asyncio
+async def test_fix_id_pattern_non_problematic_features_pass_through() -> None:
+    orig_pattern = r"USA.4.1\__"
+    assert fix_id_pattern(2, orig_pattern, "gadm", "4.1") == orig_pattern
+
+
+@pytest.mark.asyncio
+async def test_fix_id_pattern_strip_revision_sql_for_those_missing_it() -> None:
+    orig_pattern = r"IDN.35.4\__"
+    assert fix_id_pattern(2, orig_pattern, "gadm", "4.1") == "IDN.35.4"
+
+
+@pytest.mark.asyncio
+async def test_fix_id_pattern_pass_through_ghana_level_0() -> None:
+    orig_pattern = r"GHA"
+    assert fix_id_pattern(0, orig_pattern, "gadm", "4.1") == orig_pattern
+
+
+@pytest.mark.asyncio
+async def test_fix_id_pattern_remove_period_in_ghana_level_1() -> None:
+    orig_pattern = r"GHA.4\__"
+    assert fix_id_pattern(1, orig_pattern, "gadm", "4.1") == r"GHA4\__"
+
+
+@pytest.mark.asyncio
+async def test_fix_id_pattern_remove_period_in_ghana_level_2() -> None:
+    orig_pattern = r"GHA.4.1\__"
+    assert fix_id_pattern(1, orig_pattern, "gadm", "4.1") == r"GHA4.1\__"
