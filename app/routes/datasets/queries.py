b5fe785f4bfbb8b797d63a1c0616caafcfefb3d5
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-12-18 12:31:30 -0500

    Move SQL helper functions into a dedicated file.

--- app/routes/datasets/queries.py
+++ app/routes/datasets/queries.py
@@ -6,7 +6,6 @@ import re
 import uuid
 from io import StringIO
 from typing import Any, Dict, List, Optional, Tuple, Union, cast
-from urllib.parse import unquote
 from uuid import UUID, uuid4
 
 import httpx
@@ -20,10 +19,6 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.logger import logger
 from fastapi.openapi.models import APIKey
 from fastapi.responses import ORJSONResponse, RedirectResponse
-from pglast import printers  # noqa
-from pglast import Node, parse_sql
-from pglast.parser import ParseError
-from pglast.printer import RawStream
 from pydantic.tools import parse_obj_as
 
 from app.settings.globals import API_URL
@@ -35,33 +30,6 @@ from ...crud import assets
 from ...models.enum.assets import AssetType
 from ...models.enum.creation_options import Delimiters
 from ...models.enum.geostore import GeostoreOrigin
-from ...models.enum.pg_admin_functions import (
-    advisory_lock_functions,
-    backup_control_functions,
-    collation_management_functions,
-    configuration_settings_functions,
-    database_object_location_functions,
-    database_object_size_functions,
-    generic_file_access_functions,
-    index_maintenance_functions,
-    recovery_control_functions,
-    recovery_information_functions,
-    replication_sql_functions,
-    server_signaling_functions,
-    snapshot_synchronization_functions,
-    table_rewrite_information,
-)
-from ...models.enum.pg_sys_functions import (
-    access_privilege_inquiry_functions,
-    comment_information_functions,
-    committed_transaction_information,
-    control_data_functions,
-    object_information_and_addressing_functions,
-    schema_visibility_inquiry_functions,
-    session_information_functions,
-    system_catalog_information_functions,
-    transaction_ids_and_snapshots,
-)
 from ...models.enum.pixetl import Grid
 from ...models.enum.queries import QueryFormat, QueryType
 from ...models.orm.assets import Asset as AssetORM
@@ -93,6 +61,7 @@ from ...utils.decorators import hash_dict
 from ...utils.geostore import get_geostore
 from .. import dataset_version_dependency
 from . import _verify_source_file_access
+from .utils.query_helpers import normalize_sql
 
 router = APIRouter()
 
@@ -534,136 +503,6 @@ def _get_query_type(default_asset: AssetORM, geostore: Optional[GeostoreCommon])
         )
 
 
-def _has_only_one_statement(parsed: List[Dict[str, Any]]) -> None:
-    if len(parsed) != 1:
-        raise HTTPException(
-            status_code=400, detail="Must use exactly one SQL statement."
-        )
-
-
-def _is_select_statement(parsed: List[Dict[str, Any]]) -> None:
-    select = parsed[0]["RawStmt"]["stmt"].get("SelectStmt", None)
-    if not select:
-        raise HTTPException(status_code=400, detail="Must use SELECT statements only.")
-
-
-def _has_no_with_clause(parsed: List[Dict[str, Any]]) -> None:
-    with_clause = parsed[0]["RawStmt"]["stmt"]["SelectStmt"].get("withClause", None)
-    if with_clause:
-        raise HTTPException(status_code=400, detail="Must not have WITH clause.")
-
-
-def _only_one_from_table(parsed: List[Dict[str, Any]]) -> None:
-    from_clause = parsed[0]["RawStmt"]["stmt"]["SelectStmt"].get("fromClause", None)
-    if not from_clause or len(from_clause) > 1:
-        raise HTTPException(
-            status_code=400, detail="Must list exactly one table in FROM clause."
-        )
-
-
-def _no_subqueries(parsed: List[Dict[str, Any]]) -> None:
-    sub_query = parsed[0]["RawStmt"]["stmt"]["SelectStmt"]["fromClause"][0].get(
-        "RangeSubselect", None
-    )
-    if sub_query:
-        raise HTTPException(status_code=400, detail="Must not use sub queries.")
-
-
-def _no_forbidden_functions(parsed: List[Dict[str, Any]]) -> None:
-    functions = _get_item_value("FuncCall", parsed)
-
-    forbidden_function_list = [
-        configuration_settings_functions,
-        server_signaling_functions,
-        backup_control_functions,
-        recovery_information_functions,
-        recovery_control_functions,
-        snapshot_synchronization_functions,
-        replication_sql_functions,
-        database_object_size_functions,
-        database_object_location_functions,
-        collation_management_functions,
-        index_maintenance_functions,
-        generic_file_access_functions,
-        advisory_lock_functions,
-        table_rewrite_information,
-        session_information_functions,
-        access_privilege_inquiry_functions,
-        schema_visibility_inquiry_functions,
-        system_catalog_information_functions,
-        object_information_and_addressing_functions,
-        comment_information_functions,
-        transaction_ids_and_snapshots,
-        committed_transaction_information,
-        control_data_functions,
-    ]
-
-    for f in functions:
-        function_names = f["funcname"]
-        for fn in function_names:
-            function_name = fn["String"]["str"]
-
-            # block functions which start with `pg_`, `PostGIS` or `_`
-            if (
-                function_name[:3].lower() == "pg_"
-                or function_name[:1].lower() == "_"
-                or function_name[:7].lower() == "postgis"
-            ):
-                raise HTTPException(
-                    status_code=400,
-                    detail="Use of admin, system or private functions is not allowed.",
-                )
-
-            # Also block any other banished functions
-            for forbidden_functions in forbidden_function_list:
-                if function_name in forbidden_functions:
-                    raise HTTPException(
-                        status_code=400,
-                        detail="Use of admin, system or private functions is not allowed.",
-                    )
-
-
-def _no_forbidden_value_functions(parsed: List[Dict[str, Any]]) -> None:
-    value_functions = _get_item_value("SQLValueFunction", parsed)
-    if value_functions:
-        raise HTTPException(
-            status_code=400,
-            detail="Use of sql value functions is not allowed.",
-        )
-
-
-async def normalize_sql(
-    dataset: str, geometry: Geometry | None, sql: str, version: str
-) -> str:
-    try:
-        parsed = parse_sql(unquote(sql))
-    except ParseError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-    _has_only_one_statement(parsed)
-    _is_select_statement(parsed)
-    _has_no_with_clause(parsed)
-    _only_one_from_table(parsed)
-    _no_subqueries(parsed)
-    _no_forbidden_functions(parsed)
-    _no_forbidden_value_functions(parsed)
-
-    # always overwrite the table name with the current dataset version name, to make sure no other table is queried
-    parsed[0]["RawStmt"]["stmt"]["SelectStmt"]["fromClause"][0]["RangeVar"][
-        "schemaname"
-    ] = dataset
-    parsed[0]["RawStmt"]["stmt"]["SelectStmt"]["fromClause"][0]["RangeVar"][
-        "relname"
-    ] = version
-
-    if geometry:
-        parsed = await _add_geometry_filter(parsed, geometry)
-
-    # convert back to text
-    sql = RawStream()(Node(parsed))
-    return sql
-
-
 async def _query_table(
     dataset: str,
     version: str,
@@ -704,46 +543,6 @@ def _orm_to_csv(
         csv_file.seek(0)
 
     return csv_file
-
-
-def _get_item_value(key: str, parsed: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
-    """Return all functions in an AST."""
-
-    # loop through statement recursively and yield all functions
-    def walk_dict(d):
-        for k, v in d.items():
-            if k == key:
-                yield v
-            if isinstance(v, dict):
-                yield from walk_dict(v)
-            elif isinstance(v, list):
-                for _v in v:
-                    yield from walk_dict(_v)
-
-    values: List[Dict[str, Any]] = list()
-    for p in parsed:
-        values += list(walk_dict(p))
-    return values
-
-
-async def _add_geometry_filter(parsed_sql, geometry: Geometry):
-    # make empty select statement with where clause including filter
-    # this way we can later parse it as AST
-    intersect_filter = f"SELECT WHERE ST_Intersects(geom, ST_SetSRID(ST_GeomFromGeoJSON('{geometry.json()}'),4326))"
-
-    # combine the two where clauses
-    parsed_filter = parse_sql(intersect_filter)
-    filter_where = parsed_filter[0]["RawStmt"]["stmt"]["SelectStmt"]["whereClause"]
-    sql_where = parsed_sql[0]["RawStmt"]["stmt"]["SelectStmt"].get("whereClause", None)
-
-    if sql_where:
-        parsed_sql[0]["RawStmt"]["stmt"]["SelectStmt"]["whereClause"] = {
-            "BoolExpr": {"boolop": 0, "args": [sql_where, filter_where]}
-        }
-    else:
-        parsed_sql[0]["RawStmt"]["stmt"]["SelectStmt"]["whereClause"] = filter_where
-
-    return parsed_sql
 
 
 async def _query_raster(
