16b11719d6f290dd8bb087ade0b5bf980b0c3ab8
Dan Scales <dan.scales@gmail.com>
2026-03-13 14:36:09 -0700

    Add table of restricted dataset versions and deny access for those on queries.
    This is analogous to the query permission check we already have for
    restricted datasets (which has only ever been
    "wdpa_licensed_protected_areas"). I am just implementing a permission
    check for specific restricted dataset/versions as well.
    
    I already added a restricted version umd_tree_cover_loss/v1.13, so that
    will prevent access to TCL2025 (until we remove the restriction), unless
    the query supplied a bearer token with ADMIN privileges for either
    gfw-pro or gfw-apps.
    
    New test is test_query_restricted_version_disallowed, but I also made a
    re-factoring change to pull out common code from a bunch of the tests
    into a small helper function get_headers_with_origin.

--- app/authentication/token.py
+++ app/authentication/token.py
@@ -1,4 +1,4 @@
-from typing import cast
+from typing import cast, Tuple, List
 
 from fastapi import Depends, HTTPException
 from fastapi.logger import logger
@@ -6,8 +6,8 @@ from fastapi.security import OAuth2PasswordBearer
 from httpx import Response
 
 from ..models.pydantic.authentication import User
-from ..routes import dataset_dependency
-from ..settings.globals import PROTECTED_QUERY_DATASETS
+from ..routes import dataset_version_dependency
+from ..settings.globals import PROTECTED_QUERY_DATASETS, PROTECTED_QUERY_DATASET_VERSIONS
 from ..utils.rw_api import who_am_i
 
 # token dependency where we immediately cause an exception if there is no auth token
@@ -40,39 +40,40 @@ async def is_admin(token: str = Depends(oauth2_scheme)) -> bool:
     User must be ADMIN for gfw app
     """
 
-    return await is_app_admin(token, "gfw", "Unauthorized")
+    return await is_app_admin(token, ["gfw"], "Unauthorized")
 
 
-async def is_gfwpro_admin_for_query(
-    dataset: str = Depends(dataset_dependency),
+async def is_admin_for_query(
+    dv: Tuple[str, str] = Depends(dataset_version_dependency),
     token: str | None = Depends(oauth2_scheme_no_auto),
 ) -> bool:
-    """If the dataset is protected dataset, calls GFW API to authorize user by
-    requiring the user must be ADMIN for gfw-pro app.
+    """If the dataset is protected dataset or the dataset/version is a protected version, calls GFW API
+    to authorize user by requiring the user must be ADMIN for gfw or gfw-pro app.
 
     If the dataset is not protected, just returns True without any
     required token or authorization.
     """
 
-    if dataset in PROTECTED_QUERY_DATASETS:
+    dataset, version = dv
+    if dataset in PROTECTED_QUERY_DATASETS or f"{dataset}/{version}" in PROTECTED_QUERY_DATASET_VERSIONS:
         if token is None:
             raise HTTPException(
-                status_code=401, detail="Unauthorized query on a restricted dataset"
+                status_code=401, detail="Unauthorized query on a restricted dataset or version"
             )
         else:
             return await is_app_admin(
                 cast(str, token),
-                "gfw-pro",
-                error_str="Unauthorized query on a restricted dataset",
+                ["gfw", "gfw-pro"],
+                error_str="Unauthorized query on a restricted dataset or version",
             )
 
     return True
 
 
-async def is_app_admin(token: str, app: str, error_str: str) -> bool:
+async def is_app_admin(token: str, apps: List[str], error_str: str) -> bool:
     """Calls GFW API to authorize user.
 
-    User must be an ADMIN for the specified app, else it will throw an
+    User must be an ADMIN for one of the specified apps, else it will throw an
     exception with the specified error string.
     """
 
@@ -80,7 +81,7 @@ async def is_app_admin(token: str, app: str, error_str: str) -> bool:
 
     if response.status_code == 401 or not (
         response.json()["role"] == "ADMIN"
-        and app in response.json()["extraUserData"]["apps"]
+        and any(app in response.json()["extraUserData"]["apps"] for app in apps)
     ):
         logger.warning(f"ADMIN privileges required. Unauthorized user: {response.text}")
         raise HTTPException(status_code=401, detail=error_str)
